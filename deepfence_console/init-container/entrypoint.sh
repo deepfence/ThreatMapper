a3dc43afc1de98cd6a035d9eb2133d95f1fe2255
Ramanan Ravikumar <38394463+ramanan-ravi@users.noreply.github.com>
2021-11-26 16:33:53 +0530

    Remove unneeded privileged access for redis and backend container #115 (#117)

---
+++ deepfence_console/init-container/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Required for redis
+sysctl -w net.core.somaxconn=512
+sysctl vm.overcommit_memory=1
+
+# Required for deepfence-backend
+sysctl -w net.core.somaxconn=10240
+sysctl -w net.ipv4.tcp_mem="1048576 1048576 6291456"
+sysctl -w net.ipv4.tcp_max_syn_backlog=1024
+sysctl -w net.ipv4.ip_local_port_range="1024 65534"
+sysctl -w fs.nr_open=1048576
+sysctl -w fs.file-max=1048576
