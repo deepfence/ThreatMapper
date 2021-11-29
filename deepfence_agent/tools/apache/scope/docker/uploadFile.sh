9711b7eed00474ec86a4c63693be662b7bd57a94
Harshvardhan Karn <harshvardhan@deepfence.io>
2021-11-29 10:32:16 +0530

    put mountpoint as 6th var to uploadFile for serverless

--- deepfence_agent/tools/apache/scope/docker/uploadFile.sh
+++ deepfence_agent/tools/apache/scope/docker/uploadFile.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-$DF_INSTALL_DIR/usr/local/bin/fileUploader "$1" "$2" "$3" "$4" "$5" >> $DF_INSTALL_DIR/var/log/fenced/cve_upload_file.logfile 2>&1
+# Set mount dir to "/" for Serverless or Fargate
+if [ ! -v DF_SERVERLESS ]; then
+    MOUNT_PATH_VAR="/fenced/mnt/host/"
+else
+    MOUNT_PATH_VAR="/"
+fi
+
+$DF_INSTALL_DIR/usr/local/bin/fileUploader "$1" "$2" "$3" "$4" "$5" "$MOUNT_PATH_VAR" >> $DF_INSTALL_DIR/var/log/fenced/cve_upload_file.logfile 2>&1
