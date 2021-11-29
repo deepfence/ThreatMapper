4ed8a5fb74ef9f109541adfeb2c6a374202c5c47
Harshvardhan Karn <harshvardhan@deepfence.io>
2021-11-29 08:38:40 +0530

    put DF_INSTALL_DIR in fileuploader script

--- deepfence_agent/tools/apache/scope/docker/uploadFile.sh
+++ deepfence_agent/tools/apache/scope/docker/uploadFile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/local/bin/fileUploader "$1" "$2" "$3" "$4" "$5" >> /var/log/fenced/cve_upload_file.logfile 2>&1
+$DF_INSTALL_DIR/usr/local/bin/fileUploader "$1" "$2" "$3" "$4" "$5" >> $DF_INSTALL_DIR/var/log/fenced/cve_upload_file.logfile 2>&1
