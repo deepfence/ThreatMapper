80b9972b0bbae2858932b9c4d5221166bf9f7338
ramanan-ravi <ramanan@deepfence.io>
2022-02-11 23:14:08 +0530

    Run vulnerability sbom generator as supervisor job #106

--- deepfence_agent/build/plugincode-build.sh
+++ deepfence_agent/build/plugincode-build.sh
@@ -9,6 +9,12 @@ if [ $build_result -ne 0 ]
 then
     exit 1
 fi
+make bin/vulnerability_sbom_plugin
+build_result=$?
+if [ $build_result -ne 0 ]
+then
+    exit 1
+fi
 cd -
 mkdir ./plugins/docker_bin 2>/dev/null
 cp /tmp/plugins/bin/* ./plugins/docker_bin
