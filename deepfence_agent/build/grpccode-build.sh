34167f7fefe9816daa43424704c0c5d653cf6333
ramanan-ravi <ramanan@deepfence.io>
2022-02-15 14:53:38 +0530

    Generate SBOM for vulnerability scan #106

--- deepfence_agent/build/grpccode-build.sh
+++ deepfence_agent/build/grpccode-build.sh
@@ -10,5 +10,6 @@ then
     exit 1
 fi
 cd -
+rm -rf ./tools/apache/scope/proto
 mkdir ./tools/apache/scope/proto 2>/dev/null
 cp /tmp/plugins/proto/* ./tools/apache/scope/proto
--- deepfence_agent/build/plugincode-build.sh
+++ deepfence_agent/build/plugincode-build.sh
@@ -16,5 +16,6 @@ then
     exit 1
 fi
 cd -
+rm -rf ./plugins/docker_bin
 mkdir ./plugins/docker_bin 2>/dev/null
 cp /tmp/plugins/bin/* ./plugins/docker_bin
