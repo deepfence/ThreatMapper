2a80d1d00aea4fc00ad74c742c21fe7dac6d7890
ramanan-ravi <ramanan@deepfence.io>
2022-02-17 13:36:43 +0530

    Dockerfile for vulnerability mapper #106

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ DEEPFENCE_BACKEND_DIR=$(pwd)/deepfence_backend
 DEEPFENCE_UI_DIR=$(pwd)/deepfence_ui
 DEEPFENCE_DIAG_DIR=$(pwd)/deepfence_diagnosis
 DEEPFENCE_FETCHER_DIR=$DEEPFENCE_CONSOLE_DIR/clair
-DEEPAUDIT_DIR=$DEEPFENCE_CONSOLE_DIR/deepaudit
+VULNERABILITY_MAPPER_DIR=$DEEPFENCE_CONSOLE_DIR/vulnerability_mapper
 
 cd $DEEPFENCE_CONSOLE_DIR
 
@@ -21,14 +21,6 @@ else
     echo "SSL certificate found"
 fi
 
-dependency_check_file=$(pwd)/clair/dependency-check-6.5.3-release.zip
-if [ ! -f "$dependency_check_file" ]; then
-    wget https://github.com/jeremylong/DependencyCheck/releases/download/v6.5.3/dependency-check-6.5.3-release.zip -P "$(pwd)/clair/"
-    if [ ! $? -eq 0 ]; then
-        exit 1
-    fi
-fi
-
 echo "Building init container image"
 cd $DEEPFENCE_CONSOLE_DIR/init-container
 docker build -f $DEEPFENCE_CONSOLE_DIR/init-container/Dockerfile -t ${IMAGE_REPOSITORY:-deepfenceio}/deepfence_init_ce:${DF_IMG_TAG:-latest} .
@@ -40,12 +32,7 @@ fi
 
 echo "Building Vulnerability mapper image"
 cd $DEEPFENCE_CONSOLE_DIR
-rm -rf $DEEPAUDIT_DIR/filebeat $DEEPAUDIT_DIR/cve_scan_registry
-cp -r filebeat $DEEPAUDIT_DIR
-rm -rf $DEEPAUDIT_DIR/filebeat/filebeat.yml
-cp clair/dependency-check-6.5.3-release.zip $DEEPAUDIT_DIR
-cp -r $DEEPFENCE_BACKEND_DIR/cve_scan_registry $DEEPAUDIT_DIR
-docker build -f $DEEPAUDIT_DIR/Dockerfile -t ${IMAGE_REPOSITORY:-deepfenceio}/deepfence_vulnerability_mapper_ce:${DF_IMG_TAG:-latest} $DEEPAUDIT_DIR
+docker build -f $VULNERABILITY_MAPPER_DIR/Dockerfile -t ${IMAGE_REPOSITORY:-deepfenceio}/deepfence_vulnerability_mapper_ce:${DF_IMG_TAG:-latest} $VULNERABILITY_MAPPER_DIR
 
 if [ ! $? -eq 0 ]; then
     echo "Building vulnerability mapper image failed. Exiting"
