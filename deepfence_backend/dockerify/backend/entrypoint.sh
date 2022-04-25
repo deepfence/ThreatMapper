c96ac4554dd38035301f5d80336004ad8f021d38
Ramanan Ravikumar <38394463+ramanan-ravi@users.noreply.github.com>
2022-04-25 22:30:01 +0530

    Use pg_isready command to check if postgresql (#413)

--- deepfence_backend/dockerify/backend/entrypoint.sh
+++ deepfence_backend/dockerify/backend/entrypoint.sh
@@ -3,7 +3,7 @@
 # This sleep is to accommodate slow startup of deepfence-postgres container
 sleep 60
 
-until psql "host=${POSTGRES_USER_DB_HOST} port=${POSTGRES_USER_DB_PORT} sslmode=${POSTGRES_USER_DB_SSLMODE} user=${POSTGRES_USER_DB_USER} password=${POSTGRES_USER_DB_PASSWORD} dbname=${POSTGRES_USER_DB_NAME}" -c '\l'; do
+until pg_isready -h "${POSTGRES_USER_DB_HOST}" -p "${POSTGRES_USER_DB_PORT}" -U "${POSTGRES_USER_DB_USER}" -d "${POSTGRES_USER_DB_NAME}"; do
   echo >&2 "Postgres is unavailable - sleeping"
   sleep 5
 done
