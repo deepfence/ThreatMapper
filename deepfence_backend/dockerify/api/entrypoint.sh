fa0d81d2d4b9df8d5b005b016c17fd8586e3f9cd
Ramanan Ravikumar <ramanan@deepfence.io>
2022-04-25 22:28:58 +0530

    Use pg_isready command to check if postgresql

--- deepfence_backend/dockerify/api/entrypoint.sh
+++ deepfence_backend/dockerify/api/entrypoint.sh
@@ -21,7 +21,7 @@ dockerify/api/init_es_config.sh
 
 sleep 10
 
-until psql "host=${POSTGRES_USER_DB_HOST} port=${POSTGRES_USER_DB_PORT} sslmode=${POSTGRES_USER_DB_SSLMODE} user=${POSTGRES_USER_DB_USER} password=${POSTGRES_USER_DB_PASSWORD} dbname=${POSTGRES_USER_DB_NAME}" -c '\l'; do
+until pg_isready -h "${POSTGRES_USER_DB_HOST}" -p "${POSTGRES_USER_DB_PORT}" -U "${POSTGRES_USER_DB_USER}" -d "${POSTGRES_USER_DB_NAME}"; do
   echo >&2 "Postgres is unavailable - sleeping"
   sleep 5
 done
--- deepfence_console/fetcher/start_fetcher.sh
+++ deepfence_console/fetcher/start_fetcher.sh
@@ -13,7 +13,7 @@ fi
 
 sleep 30
 
-until psql "host=${POSTGRES_USER_DB_HOST} port=${POSTGRES_USER_DB_PORT} sslmode=${POSTGRES_USER_DB_SSLMODE} user=${POSTGRES_USER_DB_USER} password=${POSTGRES_USER_DB_PASSWORD} dbname=${POSTGRES_USER_DB_NAME}" -c '\l'; do
+until pg_isready -h "${POSTGRES_USER_DB_HOST}" -p "${POSTGRES_USER_DB_PORT}" -U "${POSTGRES_USER_DB_USER}" -d "${POSTGRES_USER_DB_NAME}"; do
   echo >&2 "Postgres is unavailable - sleeping"
   sleep 5
 done
