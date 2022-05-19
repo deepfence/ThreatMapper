1a7049ad7ca11f7b80b0482abb7d876e969b4e7e
Manan Vaghasiya <manan@ironmanan.com>
2022-05-19 20:05:30 +0530

    use getter functions for agent commands

--- deepfence_ui/app/scripts/components/common/agent-setup/index.jsx
+++ deepfence_ui/app/scripts/components/common/agent-setup/index.jsx
@@ -5,20 +5,25 @@ import useCopyToClipboard from 'react-use/lib/useCopyToClipboard';
 import { toaster } from '../../../actions';
 import styles from './agent-setup.module.scss';
 
-const dockerInstructions = `docker run -dit --cpus=".2" --name=deepfence-agent --restart on-failure --pid=host --net=host \\
+
+const getDockerInstructions = () => {
+  return `docker run -dit --cpus=".2" --name=deepfence-agent --restart on-failure --pid=host --net=host \\
   --privileged=true -v /sys/kernel/debug:/sys/kernel/debug:rw -v /var/log/fenced \\
   -v /var/run/docker.sock:/var/run/docker.sock -v /:/fenced/mnt/host/:ro \\
   -e USER_DEFINED_TAGS="" -e MGMT_CONSOLE_URL="${window.location.origin ?? '---CONSOLE-IP---'}" -e MGMT_CONSOLE_PORT="443" \\
   -e DEEPFENCE_KEY="${localStorage.getItem('dfApiKey') ?? '---DEEPFENCE-API-KEY---'}" \\
   deepfenceio/deepfence_agent_ce:${process.env.__PRODUCTVERSION__}
 `;
+}
 
-const k8sInstructions = `helm repo add deepfence https://deepfence-helm-charts.s3.amazonaws.com/threatmapper
+const getK8sInstructions = () => {
+  return `helm repo add deepfence https://deepfence-helm-charts.s3.amazonaws.com/threatmapper
 
-helm install deepfence-agent deepfence/deepfence-agent \\
-    --version ${process.env.__PRODUCTVERSION__} \\
-    --set managementConsoleUrl=${window.location.origin ?? '---CONSOLE-IP---'} \\
-    --set deepfenceKey=${localStorage.getItem('dfApiKey') ?? '---DEEPFENCE-API-KEY---'}`;
+  helm install deepfence-agent deepfence/deepfence-agent \\
+  --version ${process.env.__PRODUCTVERSION__} \\
+  --set managementConsoleUrl=${window.location.origin ?? '---CONSOLE-IP---'} \\
+  --set deepfenceKey=${localStorage.getItem('dfApiKey') ?? '---DEEPFENCE-API-KEY---'}`;
+};
 
 export const AgentSetup = () => {
 
@@ -27,11 +32,11 @@ export const AgentSetup = () => {
   const [state, copyToClipboard] = useCopyToClipboard();
 
   function onDockerCopyClick() {
-    copyToClipboard(dockerInstructions);
+    copyToClipboard(getDockerInstructions());
   }
 
   function onK8sCopyClick() {
-    copyToClipboard(k8sInstructions);
+    copyToClipboard(getK8sInstructions());
   }
 
   useEffect(() => {
@@ -50,7 +55,7 @@ export const AgentSetup = () => {
       </div>
       <div className={styles.codeBlock}>
         {
-          dockerInstructions
+          getDockerInstructions()
         }
         <i className={`fa fa-copy ${styles.copyButton}`} onClick={onDockerCopyClick} />
       </div>
@@ -59,7 +64,7 @@ export const AgentSetup = () => {
       </div>
       <div className={styles.codeBlock}>
         {
-          k8sInstructions
+          getK8sInstructions()
         }
         <i className={`fa fa-copy ${styles.copyButton}`} onClick={onK8sCopyClick} />
       </div>
