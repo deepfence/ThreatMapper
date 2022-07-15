476e302ee4f424dd7c1335f265bda5d4f422bfc9
Owen Garrett <owen.garrett@gmail.com>
2022-07-15 16:39:42 +0100

    github link

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -38,7 +38,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/ogarrett/ThreatMapper/docs/',
+            'https://github.com/deepfence/ThreatMapper/docs/',
           breadcrumbs: true,
           routeBasePath: '/',
           // options for remark-admonitions
