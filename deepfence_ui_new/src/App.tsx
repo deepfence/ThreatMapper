1af2fe492380a2060999ad98e353bf011617fb8b
Manan Vaghasiya <manan@ironmanan.com>
2022-08-22 16:07:25 +0530

    ui: add tailwind support

--- deepfence_ui_new/src/App.tsx
+++ deepfence_ui_new/src/App.tsx
@@ -1,32 +1,5 @@
-import './App.css';
-
-import { useState } from 'react';
-
-import reactLogo from './assets/react.svg';
-
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </div>
-  );
+  return <div className="w-full text-blue-600">Welcome to deepfence!</div>;
 }
 
 export default App;
