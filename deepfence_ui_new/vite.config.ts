c2553ed0bb9bb956ec940dfc321dcc9e3fea5ddc
Milan Deepfence <milandeepfence@Milans-MacBook-Pro.local>
2022-08-24 11:21:46 +0530

    add threshold for test

--- deepfence_ui_new/vite.config.ts
+++ deepfence_ui_new/vite.config.ts
@@ -3,17 +3,23 @@
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { configDefaults } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
-    includeSource: ['src/**/*.{ts, tsx}'],
+    includeSource: ['src/**/*.test.{ts, tsx}'],
+    exclude: [...configDefaults.exclude, 'e2e/**'],
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/tests/setup.ts'], // to do prior task before all of your tests run
     coverage: {
       reporter: ['text', 'json', 'html'],
+      lines: 100,
+      functions: 100,
+      branches: 100,
+      statements: 100,
     },
   },
 });
