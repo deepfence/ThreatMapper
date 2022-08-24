92a493bb0de108b806c9231398bb54fff7703c5a
Manan Vaghasiya <manan@ironmanan.com>
2022-08-24 15:48:42 +0530

    ui: set-up api proxy

--- deepfence_ui_new/vite.config.ts
+++ deepfence_ui_new/vite.config.ts
@@ -2,24 +2,42 @@
 /// <reference types="vite/client" />
 
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { configDefaults } from 'vitest/config';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    includeSource: ['src/**/*.test.{ts, tsx}'],
-    exclude: [...configDefaults.exclude, 'e2e/**'],
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/tests/setup.ts'], // to do prior task before all of your tests run
-    coverage: {
-      reporter: ['text', 'json', 'html'],
-      lines: 100,
-      functions: 100,
-      branches: 100,
-      statements: 100,
+export default defineConfig(({ mode }) => {
+  const viteEnv = loadEnv(mode, '.');
+  return {
+    plugins: [react()],
+    test: {
+      includeSource: ['src/**/*.test.{ts, tsx}'],
+      exclude: [...configDefaults.exclude, 'e2e/**'],
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: ['./src/tests/setup.ts'], // to do prior task before all of your tests run
+      coverage: {
+        reporter: ['text', 'json', 'html'],
+        lines: 100,
+        functions: 100,
+        branches: 100,
+        statements: 100,
+      },
     },
-  },
+    server: {
+      proxy: {
+        '^/deepfence/v1.5/.*': {
+          target: viteEnv.VITE_API_BASE_URL,
+          changeOrigin: true,
+          secure: false,
+        },
+        '^/topology-api/.*': {
+          target: viteEnv.VITE_API_BASE_URL,
+          ws: true,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  };
 });
