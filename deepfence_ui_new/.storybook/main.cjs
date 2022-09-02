7386e8d611ad0f54c1cca69f3fd025a8c9b93737
Manan Vaghasiya <manan@ironmanan.com>
2022-09-02 12:10:54 +0000

    add basic checkbox component

--- deepfence_ui_new/.storybook/main.cjs
+++ deepfence_ui_new/.storybook/main.cjs
@@ -1,18 +1,24 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
+    'storybook-addon-themes',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-vite',
   },
-  "features": {
-    "storyStoreV7": true
-  }
-}
+  features: {
+    storyStoreV7: true,
+  },
+};
--- deepfence_ui_new/.storybook/preview.cjs
+++ deepfence_ui_new/.storybook/preview.cjs
@@ -1,9 +1,40 @@
+import '../src/index.css'; // import tailwind styles
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+  themes: {
+    clearable: false,
+    list: [
+      {
+        name: 'Light',
+        class: [],
+        color: '#F9FAFB', // gray-50
+        default: true,
+      },
+      {
+        name: 'Dark',
+        class: ['dark'],
+        color: '#111928', // gray-900
+      },
+    ],
+  },
+  backgrounds: {
+    default: 'Light',
+    values: [
+      {
+        name: 'Light',
+        value: '#F9FAFB', // gray-50
+      },
+      {
+        name: 'Dark',
+        value: '#111928', // gray-900
+      },
+    ],
+  },
+};
