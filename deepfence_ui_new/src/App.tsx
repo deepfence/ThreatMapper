f5501bed8bacf6a72c2ef2f052949d17a3798096
Manan Vaghasiya <manan@ironmanan.com>
2022-09-06 07:02:22 +0000

    add tooltip component

--- deepfence_ui_new/src/App.tsx
+++ deepfence_ui_new/src/App.tsx
@@ -1,5 +1,8 @@
+import cx from 'classnames';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
+import { Tooltip } from './components/tooltip/Tooltip';
+import { Typography } from './components/typography/Typography';
 import theme from './theme/default';
 import { ThemeProvider, useThemeMode } from './theme/ThemeContext';
 
@@ -11,7 +14,17 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider value={{ theme, toggleMode }}>
-        <div>placeholder</div>
+        <Tooltip content="test tooltip">
+          <h1
+            className={cx(
+              Typography.size['xs'],
+              Typography.weight.normal,
+              'text-gray-900 text-9xl',
+            )}
+          >
+            test
+          </h1>
+        </Tooltip>
       </ThemeProvider>
     </QueryClientProvider>
   );
--- deepfence_ui_new/tailwind.config.cjs
+++ deepfence_ui_new/tailwind.config.cjs
@@ -178,6 +178,32 @@ module.exports = {
           900: '#751A3D',
         },
       },
+      keyframes: {
+        // tooltip
+        'slide-up-fade': {
+          '0%': { opacity: 0, transform: 'translateY(2px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+        'slide-right-fade': {
+          '0%': { opacity: 0, transform: 'translateX(-2px)' },
+          '100%': { opacity: 1, transform: 'translateX(0)' },
+        },
+        'slide-down-fade': {
+          '0%': { opacity: 0, transform: 'translateY(-2px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+        'slide-left-fade': {
+          '0%': { opacity: 0, transform: 'translateX(2px)' },
+          '100%': { opacity: 1, transform: 'translateX(0)' },
+        },
+      },
+      animation: {
+        // tooltip
+        'slide-up-fade': 'slide-up-fade 0.5s cubic-bezier(0.16, 1, 0.3, 1)',
+        'slide-right-fade': 'slide-right-fade 0.5s cubic-bezier(0.16, 1, 0.3, 1)',
+        'slide-down-fade': 'slide-down-fade 0.5s cubic-bezier(0.16, 1, 0.3, 1)',
+        'slide-left-fade': 'slide-left-fade 0.5s cubic-bezier(0.16, 1, 0.3, 1)',
+      },
     },
   },
   plugins: [tailwindRadix()],
