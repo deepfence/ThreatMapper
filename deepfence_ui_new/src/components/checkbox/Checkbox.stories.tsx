1b0b0049c7acde7931c228eaa34a3cfb97b4062e
Manan Vaghasiya <manan@ironmanan.com>
2022-09-15 13:48:21 +0000

    add indeterminate checkbox option

--- deepfence_ui_new/src/components/checkbox/Checkbox.stories.tsx
+++ deepfence_ui_new/src/components/checkbox/Checkbox.stories.tsx
@@ -1,4 +1,6 @@
+import { CheckedState } from '@radix-ui/react-checkbox';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { useState } from 'react';
 
 import { Checkbox } from './Checkbox';
 
@@ -12,4 +14,42 @@ const Template: ComponentStory<typeof Checkbox> = (args) => <Checkbox {...args} 
 export const Default = Template.bind({});
 Default.args = {
   label: 'Default checkbox',
+};
+
+const ControlledCheckboxTemplate: ComponentStory<typeof Checkbox> = (args) => {
+  const [checked, setChecked] = useState<CheckedState>(false);
+  return (
+    <Checkbox
+      {...args}
+      checked={checked}
+      onCheckedChange={(state) => {
+        setChecked(state);
+      }}
+    />
+  );
+};
+
+export const ControlledCheckbox = ControlledCheckboxTemplate.bind({});
+ControlledCheckbox.args = {
+  label: 'Controlled checkbox',
+};
+
+const IndeterminateCheckboxTemplate: ComponentStory<typeof Checkbox> = (args) => {
+  const [checked, setChecked] = useState<CheckedState>('indeterminate');
+
+  return (
+    <Checkbox
+      {...args}
+      name="isGoodCheckbox"
+      checked={checked}
+      onCheckedChange={(state) => {
+        setChecked(state);
+      }}
+    />
+  );
+};
+
+export const IndeterminateCheckbox = IndeterminateCheckboxTemplate.bind({});
+IndeterminateCheckbox.args = {
+  label: 'Indeterminate checkbox',
 };
--- deepfence_ui_new/src/components/checkbox/Checkbox.tsx
+++ deepfence_ui_new/src/components/checkbox/Checkbox.tsx
@@ -1,8 +1,9 @@
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
 import * as LabelPrimitive from '@radix-ui/react-label';
 import cx from 'classnames';
-import React, { useId } from 'react';
-import { FaCheck } from 'react-icons/fa';
+import { isNil } from 'lodash-es';
+import React, { useEffect, useId } from 'react';
+import { FaCheck, FaMinus } from 'react-icons/fa';
 
 import { Typography } from '../typography/Typography';
 
@@ -11,7 +12,17 @@ export type CheckboxProps = CheckboxPrimitive.CheckboxProps & {
 };
 
 export const Checkbox: React.FC<CheckboxProps> = (props) => {
-  const { className, id, label, ...rest } = props;
+  const { className, id, label, checked, onCheckedChange, ...rest } = props;
+
+  const [internalChecked, setInternalChecked] =
+    React.useState<CheckboxPrimitive.CheckedState>(checked ?? false);
+
+  useEffect(() => {
+    if (!isNil(checked)) {
+      setInternalChecked(checked);
+    }
+  }, [checked]);
+
   const internalId = useId();
 
   return (
@@ -23,13 +34,29 @@ export const Checkbox: React.FC<CheckboxProps> = (props) => {
           'focus:outline-none focus:ring-blue-200 focus:ring-2 dark:focus:ring-blue-800',
           'radix-state-unchecked:bg-gray-50 radix-state-unchecked:dark:bg-gray-700 radix-state-unchecked:border radix-state-unchecked:border-gray-300 radix-state-unchecked:dark:border-gray-600',
           'radix-state-checked:bg-blue-600',
+          {
+            'bg-blue-600': internalChecked === 'indeterminate',
+          },
           className,
         )}
         data-testid={`checkbox-${id}`}
+        checked={checked}
+        onCheckedChange={(state) => {
+          if (onCheckedChange) {
+            onCheckedChange(state);
+            return;
+          }
+          setInternalChecked(state);
+        }}
         {...rest}
       >
         <CheckboxPrimitive.Indicator>
-          <FaCheck className="h-2 w-2 self-center text-white" />
+          {internalChecked === 'indeterminate' && (
+            <FaMinus className="h-2.5 w-2.5 self-center text-white" />
+          )}
+          {internalChecked === true && (
+            <FaCheck className="h-2 w-2 self-center text-white" />
+          )}
         </CheckboxPrimitive.Indicator>
       </CheckboxPrimitive.Root>
       {label?.length ? (
