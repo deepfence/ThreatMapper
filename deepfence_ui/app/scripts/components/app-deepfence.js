aaa57ced7b53e56c404543ec689038212d292f24
Manan Vaghasiya <manan@ironmanan.com>
2022-10-18 12:15:56 +0000

    add single compliance result view via url

--- deepfence_ui/app/scripts/components/app-deepfence.js
+++ deepfence_ui/app/scripts/components/app-deepfence.js
@@ -31,6 +31,7 @@ import RegisterViaInviteView from './auth-module/register-via-invite-view/regist
 import changePasswordView from './settings-view/user-profile-view/change-password-view';
 import { isPasswordInvalidated, isUserSessionActive, isUserSessionActiveAsync } from '../helpers/auth-helper';
 import Loader from './loader';
+import { SingleComplianceResult } from './compliance-view/single-result';
 
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
@@ -154,6 +155,7 @@ class DeepFenceApp extends React.Component {
             <PrivateRoute path="/settings" component={SettingsView} />
             <PrivateRoute path="/change-password" component={changePasswordView} />
 
+            <PrivateRoute path="/compliance/result/:docId" component={SingleComplianceResult} />
             <PrivateRoute
               path="/compliance/summary/:nodeId/:checkType/:scanId/:scanType"
               component={ComplianceDetailsView}
