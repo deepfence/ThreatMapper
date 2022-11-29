3d3ab5c471c74845f126711c4027f225333c2b42
Manan Vaghasiya <manan@ironmanan.com>
2022-11-29 10:56:47 +0000

    add common tailwind preset

--- deepfence_frontend/apps/dashboard/tailwind.config.cjs
+++ deepfence_frontend/apps/dashboard/tailwind.config.cjs
@@ -1,5 +1,9 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('tailwindcss').Config} */
+const preset = require('tailwind-preset');
+
 module.exports = {
+  presets: [preset],
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
