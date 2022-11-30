85db4a29ea1924a6e0fc9410b809d3ae45492644
milan-deepfence <milan@deepfence.io>
2022-11-30 23:28:43 +0530

    added basic unit test for accordion and breadcrumb component

---
+++ deepfence_frontend/packages/ui-components/src/components/accordion/Accordion.test.tsx
@@ -0,0 +1,54 @@
+import '@testing-library/jest-dom';
+
+import { fireEvent } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+
+import { renderUI } from '../../tests/utils';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from './Accordion';
+
+const Items = ['Link 1', 'Link 2', 'Link 3'];
+
+describe(`Component Accordion`, () => {
+  it(`render correct number of accordion items`, () => {
+    const { queryByText, getByText } = renderUI(
+      <Accordion type="single">
+        {Items.map((item: string) => {
+          return (
+            <AccordionItem value={item} key={item}>
+              <AccordionTrigger>Click {item}</AccordionTrigger>
+              <AccordionContent>Content {item}</AccordionContent>
+            </AccordionItem>
+          );
+        })}
+      </Accordion>,
+    );
+    const item1 = getByText('Click Link 1', {
+      exact: false,
+    });
+    const item2 = getByText('Click Link 2', {
+      exact: false,
+    });
+    const item3 = getByText('Click Link 3', {
+      exact: false,
+    });
+    expect(item1).toBeInTheDocument();
+    expect(item2).toBeInTheDocument();
+    expect(item3).toBeInTheDocument();
+
+    let item1Content = queryByText('Content Link 1', {
+      exact: false,
+    });
+    expect(item1Content).not.toBeInTheDocument();
+    fireEvent.click(item1);
+
+    item1Content = getByText('Content Link 1', {
+      exact: false,
+    });
+    expect(item1Content).toBeInTheDocument();
+  });
+});
---
+++ deepfence_frontend/packages/ui-components/src/components/breadcrumb/Breadcrumb.test.tsx
@@ -0,0 +1,75 @@
+import '@testing-library/jest-dom';
+
+import { HiHome } from 'react-icons/hi';
+import { describe, expect, it } from 'vitest';
+
+import { renderUI } from '../../tests/utils';
+import { Breadcrumb, BreadcrumbLink } from './Breadcrumb';
+
+const Link = ({
+  children,
+  className,
+  testid,
+}: {
+  children: React.ReactNode;
+  className?: string;
+  testid?: string;
+}) => (
+  <a href="/test" className={className} data-testid={testid}>
+    {children}
+  </a>
+);
+
+describe(`Component Breadcrumb`, () => {
+  it(`render correct number of breadcrumbs with icons`, () => {
+    const { getByText, getAllByText, getByTestId } = renderUI(
+      <Breadcrumb separator={'||'}>
+        <BreadcrumbLink asChild icon={<HiHome />}>
+          <Link testid="linkOneTestId">Link One</Link>
+        </BreadcrumbLink>
+        <BreadcrumbLink asChild>
+          <Link testid="linkTwoTestId">Link Two</Link>
+        </BreadcrumbLink>
+        <BreadcrumbLink asChild>
+          <Link>Link Three</Link>
+        </BreadcrumbLink>
+      </Breadcrumb>,
+    );
+    const sep = getAllByText('||', {
+      exact: false,
+    });
+    expect(sep.length).toBe(2);
+
+    expect(
+      getByText('Link One', {
+        exact: false,
+      }),
+    ).toBeInTheDocument();
+    expect(
+      getByText('Link Two', {
+        exact: false,
+      }),
+    ).toBeInTheDocument();
+    expect(
+      getByText('Link Three', {
+        exact: true, // || seperator does not appear at the last link
+      }),
+    ).toBeInTheDocument();
+
+    // first link should have icon prefix
+
+    const firstLink = getByTestId('linkOneTestId');
+    expect(firstLink).toBeInTheDocument();
+
+    const firstSvgIcon = firstLink.querySelector('svg');
+    expect(firstSvgIcon).not.toBe(null);
+
+    // second link should not have icon prefix
+
+    const secondLink = getByTestId('linkTwoTestId');
+    expect(secondLink).toBeInTheDocument();
+
+    const secondSvgIcon = secondLink.querySelector('svg');
+    expect(secondSvgIcon).toBe(null);
+  });
+});
