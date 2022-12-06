50d922d021017ef5ec22fe056cc2adc8c43f2851
Manan Vaghasiya <manan@ironmanan.com>
2022-12-06 11:59:24 +0000

    move react icons to peer dependancy

--- deepfence_frontend/apps/dashboard/src/App.tsx
+++ deepfence_frontend/apps/dashboard/src/App.tsx
@@ -1,11 +1,22 @@
 import './index.css';
 
+import { HiChevronDoubleRight, HiHome } from 'react-icons/hi';
+import { Breadcrumb, BreadcrumbLink } from 'ui-components';
+
 import { Login } from './pages/Login';
 import { ThemeProvider } from './theme/ThemeContext';
 
 function App() {
   return (
     <ThemeProvider value={{}}>
+      <Breadcrumb separator={<HiChevronDoubleRight />}>
+        <BreadcrumbLink asChild icon={<HiHome />}>
+          <a href="/test">Link One</a>
+        </BreadcrumbLink>
+        <BreadcrumbLink asChild>
+          <a href="/test">a Two</a>
+        </BreadcrumbLink>
+      </Breadcrumb>
       <Login />
     </ThemeProvider>
   );
