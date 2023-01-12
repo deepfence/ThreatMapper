efd6c40d5f97523facc10e2730c981bfc8515d2c
milan-deepfence <milan@deepfence.io>
2023-01-12 18:52:47 +0530

    feat(onboard):lets get started page added

---
+++ deepfence_frontend/apps/dashboard/src/features/onboard/components/ConnectorHeader.tsx
@@ -0,0 +1,92 @@
+import cx from 'classnames';
+import { HiChevronRight } from 'react-icons/hi';
+import { Link, useLocation } from 'react-router-dom';
+import { Breadcrumb, BreadcrumbLink, Typography } from 'ui-components';
+
+type ConnectorHeaderProps = {
+  title: string;
+  description: string;
+};
+
+const canRoute = (pathname: string) => {
+  const path = {
+    addConnector: '',
+    scanResult: '',
+    viewResult: '',
+  };
+  if (pathname.includes('view-scan-results')) {
+    path.viewResult = '/onboard/view-scan-results';
+    path.scanResult = '/onboard/scan-infrastructure';
+    path.addConnector = '/onboard/connectors';
+  } else if (pathname.includes('scan-infrastructure')) {
+    path.scanResult = '/onboard/scan-infrastructure';
+    path.addConnector = '/onboard/connectors';
+    path.viewResult = '#';
+  } else if (pathname.includes('connectors')) {
+    path.addConnector = '/onboard/connectors';
+    path.scanResult = '#';
+    path.viewResult = '#';
+  }
+  return path;
+};
+
+export const ConnectorHeader = ({ title, description }: ConnectorHeaderProps) => {
+  const location = useLocation();
+
+  const isAddConnectorRoutePath = () => {
+    return location.pathname.includes('connectors');
+  };
+
+  const isScanRoutePath = () => {
+    return location.pathname.includes('scan-infrastructure');
+  };
+
+  const isViewResultsRoutePath = () => {
+    return location.pathname.includes('view-scan-results');
+  };
+
+  return (
+    <div className="pt-6 mb-8">
+      <div className="mb-4 -mx-5">
+        <Breadcrumb separator={<HiChevronRight />} transparent>
+          <BreadcrumbLink>
+            <Link
+              to={canRoute(location.pathname).addConnector}
+              className={cx({
+                ['text-blue-600']: isAddConnectorRoutePath(),
+              })}
+            >
+              Add a connector
+            </Link>
+          </BreadcrumbLink>
+          <BreadcrumbLink>
+            <Link
+              to={canRoute(location.pathname).scanResult}
+              className={cx({
+                ['text-blue-600']: isScanRoutePath(),
+              })}
+            >
+              Scan Infrastructure
+            </Link>
+          </BreadcrumbLink>
+          <BreadcrumbLink>
+            <Link
+              to={canRoute(location.pathname).viewResult}
+              className={cx({
+                ['text-blue-600']: isViewResultsRoutePath(),
+              })}
+            >
+              View Scan Results
+            </Link>
+          </BreadcrumbLink>
+        </Breadcrumb>
+      </div>
+      <h1 className={`text-[32px] text-black dark:text-white`}>{title}</h1>
+      <p
+        className={`${Typography.size.base} ${Typography.weight.normal} mt-1.5 mb-4 dark:text-gray-400 text-gray-900`}
+      >
+        {description}
+      </p>
+    </div>
+  );
+};
--- deepfence_frontend/apps/dashboard/src/features/onboard/layouts/OnboardLayout.tsx
+++ deepfence_frontend/apps/dashboard/src/features/onboard/layouts/OnboardLayout.tsx
@@ -1,6 +1,16 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, redirect } from 'react-router-dom';
 
 import { OnboardAppHeader } from '../components/OnBoardAppHeader';
+
+export const rootOnboardLoader = async ({ request }: any) => {
+  const index = request.url.lastIndexOf('/');
+  const subpath = request.url.substring(index);
+
+  if (subpath === '/onboard' || subpath === '/onboard/') {
+    return redirect('/onboard/connectors', 302);
+  }
+  return null;
+};
 
 export const OnboardLayout = () => {
   return (
