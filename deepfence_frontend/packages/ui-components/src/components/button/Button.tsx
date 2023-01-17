3fc3d6111b92468c65136d9c9137437d25516f76
milan-deepfence <milan@deepfence.io>
2023-01-17 14:37:38 +0530

    fix(cva):converted to cva styles, rename variable for cva of text input

--- deepfence_frontend/packages/ui-components/src/components/button/Button.tsx
+++ deepfence_frontend/packages/ui-components/src/components/button/Button.tsx
@@ -1,69 +1,260 @@
-import cx from 'classnames';
+import { cva, VariantProps } from 'cva';
 import React, { ComponentProps, useId } from 'react';
 import { twMerge } from 'tailwind-merge';
 
-import { Typography } from '@/components/typography/Typography';
+import { ObjectWithNonNullableValues } from '@/types/utils';
 
-export type ButtonShape = 'default';
 export type ColorType = 'default' | 'primary' | 'danger' | 'success' | 'normal';
 export type SizeType = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 
-interface ButtonProps extends Omit<ComponentProps<'button'>, 'color'> {
+export const buttonCva = cva(
+  [
+    'font-medium',
+    'disabled:cursor-not-allowed',
+    'flex flex-row items-center justify-center',
+    'rounded-lg focus:outline-none select-none',
+    'rounded-lg focus:outline-none select-none',
+  ],
+  {
+    variants: {
+      size: {
+        xs: 'text-xs px-3 py-2',
+        sm: 'text-sm px-3 py-2',
+        md: 'text-base px-5 py-2.5',
+        lg: 'text-lg px-5 py-3',
+        xl: 'text-xl px-6 py-3.5',
+      },
+      color: {
+        default: [
+          // bg styles
+          'bg-gray-200  hover:bg-gray-300 ',
+          // text styles
+          'text-gray-800 hover:text-gray-900 ',
+          // focus styles
+          'focus:ring-2 focus:ring-gray-100',
+        ],
+        primary: [
+          // bg styles
+          'bg-blue-700 hover:bg-blue-800',
+          // text styles
+          'text-white',
+          // focus styles
+          'focus:ring-2 focus:ring-blue-200',
+        ],
+        danger: [
+          // bg styles
+          'bg-red-700 hover:bg-red-800',
+          // text styles
+          'text-white',
+          // focus styles
+          'focus:ring-2 focus:ring-red-200',
+        ],
+        success: [
+          // bg styles
+          'bg-green-700 hover:bg-green-800',
+          // text styles
+          'text-white',
+          // focus styles
+          'focus:ring-2 focus:ring-green-200',
+        ],
+        normal: [
+          // bg styles
+          'bg-white hover:bg-gray-300  ',
+          // text styles
+          'text-gray-800 hover:text-gray-800 focus:text-gray-800 ',
+          // focus styles
+          'dark:ring-gray-100 focus:ring-2 focus:ring-gray-100',
+        ],
+      },
+      pill: {
+        true: 'rounded-full',
+      },
+      withOutline: {
+        true: 'bg-white',
+      },
+    },
+    defaultVariants: {
+      color: 'default',
+      size: 'md',
+    },
+    compoundVariants: [
+      {
+        color: 'default',
+        withOutline: true,
+        className: [
+          // bg styles
+          'bg-transparent hover:bg-gray-300 ',
+          // text styles
+          'text-gray-900  dark:text-white hover:text-black dark:hover:text-black',
+          // ring styles
+          'ring-1 ring-gray-900 focus:ring-1 focus:ring-gray-200 dark:ring-white',
+        ],
+      },
+      {
+        color: 'primary',
+        withOutline: true,
+        className: [
+          // bg styles
+          'hover:bg-blue-800',
+          // text styles
+          'text-blue-700 hover:text-white',
+          // ring styles
+          'ring-1 ring-blue-700 focus:ring-2 focus:ring-blue-200',
+        ],
+      },
+      {
+        color: 'danger',
+        withOutline: true,
+        className: [
+          // bg styles
+          'hover:bg-red-800',
+          // text styles
+          'text-red-700 hover:text-white',
+          // ring styles
+          'ring-1 focus:ring-2 ring-red-700 focus:ring-red-200',
+        ],
+      },
+      {
+        color: 'success',
+        withOutline: true,
+        className: [
+          // bg styles
+          'hover:bg-green-800',
+          // text styles
+          ' text-green-700 hover:text-white',
+          // ring styles
+          'ring-1 ring-green-700 focus:ring-2 focus:ring-green-200',
+        ],
+      },
+      {
+        color: 'normal',
+        withOutline: true,
+        className: [
+          // bg styles
+          'dark:bg-gray-800 hover:bg-gray-100 hover:dark:bg-gray-700',
+          // text styles
+          'text-gray-700 dark:text-gray-400 hover:text-gray-900 focus:text-gray-900',
+          // ring styles
+          'ring-1 focus:ring-2 focus:ring-gray-200 dark:ring-gray-600 ring-gray-200',
+        ],
+      },
+    ],
+  },
+);
+
+interface ButtonProps
+  extends Omit<ComponentProps<'button'>, 'color'>,
+    ObjectWithNonNullableValues<Omit<VariantProps<typeof buttonCva>, 'withOutline'>> {
   size?: SizeType;
   startIcon?: React.ReactNode;
   endIcon?: React.ReactNode;
-  shape?: ButtonShape;
   outline?: boolean;
   color?: ColorType;
   className?: string;
 }
 
-export const classes = {
-  disabled: 'cursor-not-allowed',
-  pill: 'rounded-full',
-  size: {
-    xs: `${Typography.size.xs} px-3 py-2`,
-    sm: `${Typography.size.sm} px-3 py-2`,
-    md: `${Typography.size.base} px-5 py-2.5`,
-    lg: `${Typography.size.lg} px-5 py-3`,
-    xl: `${Typography.size.xl} px-6 py-3.5`,
+const iconCva = cva('', {
+  variants: {
+    size: {
+      xs: '',
+      sm: '',
+      md: '',
+      lg: '',
+      xl: '',
+    },
+    withStartIcon: {
+      true: '',
+    },
+    withEndIcon: {
+      true: '',
+    },
   },
-  color: {
-    default:
-      'bg-gray-200 text-gray-800 hover:bg-gray-300 hover:text-gray-900 focus:ring-2 focus:ring-gray-100',
-    primary: 'bg-blue-700 text-white hover:bg-blue-800 focus:ring-2 focus:ring-blue-200',
-    danger: 'bg-red-700 text-white hover:bg-red-800 focus:ring-2 focus:ring-red-200',
-    success:
-      'bg-green-700 text-white hover:bg-green-800 focus:ring-2 focus:ring-green-200',
-    normal:
-      'bg-white hover:bg-gray-300 text-gray-800 hover:text-gray-800 dark:ring-gray-100 focus:text-gray-800 focus:ring-2 focus:ring-gray-100',
-  },
-  outline: {
-    default:
-      'bg-white text-gray-800 ring-1 ring-gray-900 hover:bg-gray-800 hover:text-white focus:ring-1 focus:ring-gray-200 dark:ring-white',
-    primary:
-      'bg-white ring-1 ring-blue-700 text-blue-700 hover:bg-blue-800 hover:text-white focus:ring-2 focus:ring-blue-200',
-    danger:
-      'text-red-700 ring-1 ring-red-700 hover:bg-red-800 hover:text-white focus:ring-2 focus:ring-red-200',
-    success:
-      'text-green-700 ring-1 ring-green-700  hover:bg-green-800 hover:text-white focus:ring-2 focus:ring-green-200',
-    normal:
-      'bg-white dark:bg-gray-800 ring-1 ring-gray-200  hover:bg-gray-100 hover:dark:bg-gray-700 text-gray-700 dark:text-gray-400 hover:text-gray-900 focus:text-gray-900 focus:ring-2 focus:ring-gray-200 focus:ring-gray-600 dark:ring-gray-600',
-  },
-  startIcon: {
-    xs: 'mr-[10.4px]',
-    sm: 'mr-[10.4px]',
-    md: 'mr-[11px]',
-    lg: 'mr-[15px]',
-    xl: 'mr-[15px]',
-  },
-  endIcon: {
-    xs: 'ml-[10.4px]',
-    sm: 'ml-[10.4px]',
-    md: 'ml-[11px]',
-    lg: 'ml-[15px]',
-    xl: 'ml-[15px]',
-  },
+  compoundVariants: [
+    {
+      size: ['xs', 'sm'],
+      withStartIcon: true,
+      className: 'mr-[10.4px]',
+    },
+    {
+      size: 'md',
+      withStartIcon: true,
+      className: 'mr-[11px]',
+    },
+    {
+      size: ['lg', 'xl'],
+      withStartIcon: true,
+      className: 'mr-[15px]',
+    },
+    {
+      size: ['xs', 'sm'],
+      withEndIcon: true,
+      className: 'ml-[10.4px]',
+    },
+    {
+      size: 'md',
+      withEndIcon: true,
+      className: 'ml-[11px]',
+    },
+    {
+      size: ['lg', 'xl'],
+      withEndIcon: true,
+      className: 'ml-[15px]',
+    },
+    {
+      size: ['xs', 'sm'],
+      withStartIcon: true,
+      withEndIcon: true,
+      className: 'mr-[10.4px] ml-[10.4px]',
+    },
+    {
+      size: 'md',
+      withStartIcon: true,
+      withEndIcon: true,
+      className: 'mr-[11px] ml-[11px]',
+    },
+    {
+      size: ['lg', 'xl'],
+      withStartIcon: true,
+      withEndIcon: true,
+      className: 'mr-[15px] ml-[15px]',
+    },
+  ],
+});
+
+interface IconProps extends VariantProps<typeof iconCva> {
+  startIcon?: React.ReactNode;
+  endIcon?: React.ReactNode;
+  id?: string;
+}
+
+const StartIcon = ({ id, startIcon, endIcon, size }: IconProps) => {
+  return (
+    <span
+      className={iconCva({
+        size,
+        withStartIcon: !!startIcon,
+        withEndIcon: !!endIcon,
+      })}
+      data-testid={`button-icon-start-${id}`}
+    >
+      {startIcon}
+    </span>
+  );
+};
+
+const EndIcon = ({ id, size, startIcon, endIcon }: IconProps) => {
+  return (
+    <span
+      className={iconCva({
+        size,
+        withStartIcon: !!startIcon,
+        withEndIcon: !!endIcon,
+      })}
+      data-testid={`button-icon-end-${id}`}
+    >
+      {endIcon}
+    </span>
+  );
 };
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
@@ -92,52 +283,21 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         data-testid={`button-${_id}`}
         disabled={disabled}
         className={twMerge(
-          cx(
-            'flex flex-row items-center justify-center',
-            `${Typography.weight.medium}`,
-            `${classes.size[size]}`,
-            'rounded-lg focus:outline-none select-none',
-            {
-              [classes.color.primary]: color === 'primary' && !outline,
-              [classes.outline.primary]: outline && color === 'primary',
-
-              [classes.color.default]:
-                (color === undefined && !outline) || (color === 'default' && !outline),
-              [classes.outline.default]:
-                (color === undefined && outline) || (color === 'default' && outline),
-
-              [classes.color.danger]: color === 'danger' && !outline,
-              [classes.outline.danger]: color === 'danger' && outline,
-
-              [classes.color.success]: color === 'success' && !outline,
-              [classes.outline.success]: color === 'success' && outline,
-
-              [classes.color.normal]: color === 'normal' && !outline,
-              [classes.outline.normal]: color === 'normal' && outline,
-
-              [classes.disabled]: disabled,
-            },
-          ),
+          buttonCva({
+            size,
+            color,
+            withOutline: outline,
+          }),
           className,
         )}
         {...props}
       >
         {startIcon && (
-          <span
-            className={cx(classes.startIcon[size])}
-            data-testid={`button-icon-start-${_id}`}
-          >
-            {startIcon}
-          </span>
+          <StartIcon startIcon={startIcon} endIcon={endIcon} id={_id} size={size} />
         )}
         {children}
         {endIcon && (
-          <span
-            className={cx(classes.endIcon[size])}
-            data-testid={`button-icon-end-${_id}`}
-          >
-            {endIcon}
-          </span>
+          <EndIcon startIcon={startIcon} endIcon={endIcon} id={_id} size={size} />
         )}
       </button>
     );
--- deepfence_frontend/packages/ui-components/src/components/button/IconButton.tsx
+++ deepfence_frontend/packages/ui-components/src/components/button/IconButton.tsx
@@ -1,39 +1,162 @@
-import cx from 'classnames';
+import { cva, VariantProps } from 'cva';
 import { ComponentProps, forwardRef, useId } from 'react';
 import { IconContext } from 'react-icons';
+import { twMerge } from 'tailwind-merge';
 
-import {
-  classes as buttonClasses,
-  ColorType,
-  SizeType,
-} from '@/components/button/Button';
+import { ObjectWithNonNullableValues } from '@/types/utils';
 
-interface Props extends Omit<ComponentProps<'button'>, 'className' | 'color'> {
-  size?: SizeType;
+export type ColorType = 'default' | 'primary' | 'danger' | 'success' | 'normal';
+export type SizeType = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+
+export const iconButtonCVA = cva(
+  [
+    'flex flex-row items-center justify-center',
+    'rounded-full focus:outline-none select-none',
+    'disabled:cursor-not-allowed',
+  ],
+  {
+    variants: {
+      size: {
+        xs: `p-[9px]`,
+        sm: `p-[13px]`,
+        md: `p-[15px]`,
+        lg: `p-[18px]`,
+        xl: `p-[20px]`,
+      },
+      color: {
+        default: [
+          // bg styles
+          'bg-gray-200  hover:bg-gray-300 ',
+          // text styles
+          'text-gray-800 hover:text-gray-900 ',
+          // focus styles
+          'focus:ring-2 focus:ring-gray-100',
+        ],
+        primary: [
+          // bg styles
+          'bg-blue-700  hover:bg-blue-800',
+          // text styles
+          'text-white',
+          // focus styles
+          'focus:ring-2 focus:ring-blue-200',
+        ],
+        danger: [
+          // bg styles
+          'bg-red-700 hover:bg-red-800',
+          // text styles
+          'text-white',
+          // focus styles
+          'focus:ring-2 focus:ring-red-200',
+        ],
+        success: [
+          // bg styles
+          'bg-green-700  hover:bg-green-800',
+          // text styles
+          'text-white',
+          // focus styles
+          'focus:ring-2 focus:ring-green-200',
+        ],
+        normal: [
+          // bg styles
+          'bg-white hover:bg-gray-300  ',
+          // text styles
+          'text-gray-800 hover:text-gray-800 focus:text-gray-800 ',
+          // focus styles
+          'dark:ring-gray-100 focus:ring-2 focus:ring-gray-100',
+        ],
+      },
+      withOutline: {
+        true: 'bg-white',
+      },
+    },
+    defaultVariants: {
+      color: 'default',
+      size: 'md',
+    },
+    compoundVariants: [
+      {
+        color: 'default',
+        withOutline: true,
+        className: [
+          'bg-transparent hover:bg-gray-300 ',
+          // text styles
+          'text-gray-900  dark:text-white hover:text-black dark:hover:text-black',
+          // ring styles
+          'ring-1 ring-gray-900 focus:ring-1 focus:ring-gray-200 dark:ring-white',
+        ],
+      },
+      {
+        color: 'primary',
+        withOutline: true,
+        className: [
+          // bg styles
+          'hover:bg-blue-800',
+          // text styles
+          'text-blue-700 hover:text-white',
+          // ring styles
+          'ring-1 ring-blue-700 focus:ring-2 focus:ring-blue-200',
+        ],
+      },
+      {
+        color: 'danger',
+        withOutline: true,
+        className: [
+          // bg styles
+          'hover:bg-red-800',
+          // text styles
+          'text-red-700 hover:text-white',
+          // ring styles
+          'ring-1 focus:ring-2 ring-red-700 focus:ring-red-200',
+        ],
+      },
+      {
+        color: 'success',
+        withOutline: true,
+        className: [
+          // bg styles
+          'hover:bg-green-800',
+          // text styles
+          ' text-green-700 hover:text-white',
+          // ring styles
+          'ring-1 ring-green-700 focus:ring-2 focus:ring-green-200',
+        ],
+      },
+      {
+        color: 'normal',
+        withOutline: true,
+        className: [
+          // bg styles
+          'dark:bg-gray-800 hover:bg-gray-100 hover:dark:bg-gray-700',
+          // text styles
+          'text-gray-700 dark:text-gray-400 hover:text-gray-900 focus:text-gray-900',
+          // ring styles
+          'ring-1 focus:ring-2 focus:ring-gray-200 dark:ring-gray-600 ring-gray-200',
+        ],
+      },
+    ],
+  },
+);
+
+interface IconButtonProps
+  extends Omit<ComponentProps<'button'>, 'className' | 'color'>,
+    ObjectWithNonNullableValues<Omit<VariantProps<typeof iconButtonCVA>, 'withOutline'>> {
   icon?: React.ReactNode;
   outline?: boolean;
-  color?: ColorType;
 }
 
-const classes = {
-  ...buttonClasses,
-  size: {
-    xs: `p-[9px]`,
-    sm: `p-[13px]`,
-    md: `p-[15px]`,
-    lg: `p-[18px]`,
-    xl: `p-[20px]`,
+const iconCva = cva('', {
+  variants: {
+    size: {
+      xs: 'w-2.5 h-2.5',
+      sm: 'w-2.5 h-2.5',
+      md: 'w-2.5 h-2.5',
+      lg: 'w-3 h-3',
+      xl: 'w-3 h-3',
+    },
   },
-  icon: {
-    xs: 'w-2.5 h-2.5',
-    sm: 'w-2.5 h-2.5',
-    md: 'w-2.5 h-2.5',
-    lg: 'w-3 h-3',
-    xl: 'w-3 h-3',
-  },
-};
+});
 
-const IconButton = forwardRef<HTMLButtonElement, Props>(
+const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
   ({ size = 'md', color, disabled, outline, icon, id, ...props }, ref) => {
     const internalId = useId();
     const _id = id ? id : internalId;
@@ -44,34 +167,23 @@ const IconButton = forwardRef<HTMLButtonElement, Props>(
         id={_id}
         data-testid={`icon-button-${_id}`}
         disabled={disabled}
-        className={cx(
-          'flex flex-row items-center justify-center',
-          `${classes.size[size]}`,
-          'rounded-full focus:outline-none select-none',
-          {
-            [classes.color.primary]: color === 'primary' && !outline,
-            [classes.outline.primary]: outline && color === 'primary',
-
-            [classes.color.default]:
-              (color === undefined && !outline) || (color === 'default' && !outline),
-            [classes.outline.default]:
-              (color === undefined && outline) || (color === 'default' && outline),
-
-            [classes.color.danger]: color === 'danger' && !outline,
-            [classes.outline.danger]: color === 'danger' && outline,
-
-            [classes.color.success]: color === 'success' && !outline,
-            [classes.outline.success]: color === 'success' && outline,
-
-            [classes.disabled]: disabled,
-            'dark:text-white dark:bg-gray-900 dark:hover:bg-gray-800 dark:focus:ring-2 dark:focus:ring-gray-400':
-              outline,
-          },
+        className={twMerge(
+          iconButtonCVA({
+            size,
+            color,
+            withOutline: outline,
+          }),
         )}
         {...props}
       >
         {icon && (
-          <IconContext.Provider value={{ className: cx(classes.icon[size]) }}>
+          <IconContext.Provider
+            value={{
+              className: iconCva({
+                size,
+              }),
+            }}
+          >
             {icon}
           </IconContext.Provider>
         )}
