5c9a7b7dfa9d8bc2a39b9eb9463ee2ec734e55f4
milan-deepfence <milan@deepfence.io>
2023-01-23 14:05:37 +0530

    added scan in progress loader

--- deepfence_frontend/apps/dashboard/src/features/onboard/components/scan-summary/ScanInProgress.tsx
+++ deepfence_frontend/apps/dashboard/src/features/onboard/components/scan-summary/ScanInProgress.tsx
@@ -1,121 +1,106 @@
 import './scan.css';
 
-export const ScanInProgress = () => {
+export const ScanInProgress = ({ text }: { text: string }) => {
   return (
     <div className="flex flex-col items-center">
       <div className="scan-summary relative flex justify-center items-center">
         <svg xmlns="http://www.w3.org/2000/svg" width="300" height="300">
           <circle
-            id="arc2"
+            id="cir2"
             className="circle"
             cx="150"
             cy="150"
             r="120"
             opacity=".49"
             fill="none"
-            stroke="#632b26"
             strokeWidth="8"
             strokeLinecap="square"
             strokeOpacity=".99213"
             paintOrder="fill markers stroke"
           />
           <circle
-            id="arc3"
+            id="cir3"
             className="circle"
             cx="150"
             cy="150"
             r="100"
             opacity=".49"
             fill="none"
-            stroke="#632b26"
             strokeWidth="15"
             strokeLinecap="square"
             strokeOpacity=".99213"
             paintOrder="fill markers stroke"
           />
           <circle
-            id="arc4"
+            id="cir4"
             className="circle"
             cx="150"
             cy="150"
             r="120"
             opacity=".49"
             fill="none"
-            stroke="#632b26"
             strokeWidth="20"
             strokeLinecap="square"
             strokeOpacity=".99213"
             paintOrder="fill markers stroke"
           />
           <circle
-            id="arc5"
+            id="cir5"
             className="circle"
             cx="150"
             cy="150"
             r="100"
             opacity=".89"
             fill="none"
-            stroke="#632b26"
             strokeWidth="8"
             strokeLinecap="square"
             strokeOpacity=".99213"
             paintOrder="fill markers stroke"
           />
           <circle
-            id="arc6"
+            id="cir6"
             className="circle"
             cx="150"
             cy="150"
             r="90"
             opacity=".49"
             fill="none"
-            stroke="#632b26"
             strokeWidth="16"
             strokeLinecap="square"
             strokeOpacity=".99213"
             paintOrder="fill markers stroke"
           />
           <circle
-            id="arc7"
+            id="cir7"
             className="circle"
             cx="150"
             cy="150"
             r="90"
             opacity=".89"
             fill="none"
-            stroke="#632b26"
             strokeWidth="8"
             strokeLinecap="square"
             strokeOpacity=".99213"
             paintOrder="fill markers stroke"
           />
           <circle
-            id="arc8"
+            id="cir8"
             className="circle"
             cx="150"
             cy="150"
             r="80"
             opacity=".79"
-            fill="#4DD0E1"
             fillOpacity="0"
-            stroke="#632b26"
             strokeWidth="8"
             strokeLinecap="square"
             strokeOpacity=".99213"
             paintOrder="fill markers stroke"
           />
         </svg>
-        <span className="absolute text-blue-500 animate-bounce dark:text-blue-400">
-          Scanning...
-        </span>
+        <span className="absolute text-blue-500 animate-bounce">Scanning...</span>
       </div>
       <section className="text-center mt-4">
-        <h2 className="text-base font-medium text-gray-700 dark:text-gray-400">
-          Your Compliance Scan is currently running...
-        </h2>
-        <p className="text-gray-500 text-sm">
-          We will show you the result when the scan is complete
-        </p>
+        <h2 className="text-base font-medium text-gray-700 dark:text-gray-400">{text}</h2>
       </section>
     </div>
   );
--- deepfence_frontend/apps/dashboard/src/features/onboard/pages/ComplianceScanSummary.tsx
+++ deepfence_frontend/apps/dashboard/src/features/onboard/pages/ComplianceScanSummary.tsx
@@ -1,42 +1,34 @@
 import cx from 'classnames';
 import { useState } from 'react';
-import { Button } from 'ui-components';
 import { Card, Separator, Typography } from 'ui-components';
 
 import LogoAws from '@/assets/logo-aws.svg';
 import { ConnectorHeader } from '@/features/onboard/components/ConnectorHeader';
 import { ScanInProgress } from '@/features/onboard/components/scan-summary/ScanInProgress';
-import { useTheme } from '@/theme/ThemeContext';
+import { Link } from 'react-router-dom';
 
 const color: { [key: string]: string } = {
-  alarm: '#84E1BC',
-  info: '#A4CAFE',
-  ok: '#CABFFD',
-  skip: '#FACA15',
+  alarm: 'bg-red-400 dark:bg-red-500',
+  info: 'bg-blue-400 dark:bg-blue-500',
+  ok: 'bg-green-400 dark:bg-green-500',
+  skip: 'bg-gray-400 dark:bg-gray-500',
 };
 
-const colorDark: { [key: string]: string } = {
-  alarm: '#057A55',
-  info: '#1C64F2',
-  ok: '#7E3AF2',
-  skip: '#9F580A',
-};
-
-type ScanDataType = {
+type ScanType = {
   type: string;
   percentage: string;
-  values: ResultType[];
+  values: SeverityType[];
 };
-type ResultType = {
+type SeverityType = {
   name: string;
   value: number;
 };
-type ScanResultType = {
+type ScanDataType = {
   accountId: string;
-  data: ScanDataType[];
+  data: ScanType[];
 };
 
-const data: ScanResultType[] = [
+const data: ScanDataType[] = [
   {
     accountId: 'AWS123456',
     data: [
@@ -73,6 +65,14 @@ const data: ScanResultType[] = [
           {
             name: 'Info',
             value: 20,
+          },
+          {
+            name: 'Ok',
+            value: 0,
+          },
+          {
+            name: 'Skip',
+            value: 0,
           },
         ],
       },
@@ -129,12 +129,18 @@ const data: ScanResultType[] = [
   },
 ];
 
-const AccountSection = ({ accountId }: { accountId: string }) => {
+const AccountComponent = ({ accountId }: { accountId: string }) => {
   return (
-    <div className="border-r dark:border-gray-700 bg-gray-100 dark:bg-gray-700 h-full flex flex-col items-center justify-center gap-y-1">
-      <img src={LogoAws} alt="logo" height={30} width={30} />
+    <div
+      className={cx(
+        'h-full flex flex-col items-center justify-center gap-y-3',
+        'border-r dark:border-gray-700',
+        'bg-gray-100 dark:bg-gray-700',
+      )}
+    >
+      <img src={LogoAws} alt="logo" height={40} width={40} />
       <data
-        className={`${Typography.size.base} ${Typography.weight.normal} text-gray-700 dark:text-gray-200`}
+        className={`${Typography.size.base} ${Typography.weight.normal} text-gray-700 dark:text-gray-300`}
       >
         {accountId}
       </data>
@@ -142,7 +148,7 @@ const AccountSection = ({ accountId }: { accountId: string }) => {
   );
 };
 
-const PercentAndTypSection = ({
+const TypeAndPercentageComponent = ({
   type,
   percentage,
 }: {
@@ -157,56 +163,47 @@ const PercentAndTypSection = ({
       )}
     >
       <div className="flex flex-col gap-y-1 md:min-w-[100px] lg:min-w-[200px]">
-        <data className={'text-2xl text-gray-700 font-semibold dark:text-gray-400'}>
-          {type}
-        </data>
+        <data className={'text-2xl text-gray-700 dark:text-gray-300'}>{type}</data>
       </div>
       <div className="flex flex-col gap-y-1">
         <data className="text-sm text-gray-500 dark:text-gray-400">
           Overall Percentage
         </data>
-        <data className={'text-2xl text-gray-900 font-bold dark:text-white'}>
-          {percentage}
-        </data>
+        <data className={'text-2xl text-gray-700 dark:text-gray-300'}>{percentage}</data>
       </div>
     </div>
   );
 };
 
-const ChartSection = ({ values }: { values: ResultType[] }) => {
-  const { mode } = useTheme();
-  const sum = values.reduce((acc: number, scanObject: ResultType) => {
-    acc = acc + scanObject.value;
-    return acc;
-  }, 0);
+const ChartComponent = ({ values }: { values: SeverityType[] }) => {
+  const maxValue = Math.max(...values.map((v) => v.value));
+
   return (
-    <div className="">
+    <div>
       {values.map(({ name, value }) => {
-        const barWidth = Math.ceil(value === 0 ? 0 : (value / sum) * 100);
         return (
-          <div className="flex items-center w-full ml-0 lg:ml-10" key={name}>
-            <span className="pr-4 text-sm min-w-[60px] text-gray-500 text-end dark:text-gray-400">
+          <div className="flex items-center w-full" key={name}>
+            <data
+              className="pr-2 text-sm min-w-[60px] text-gray-500 text-end dark:text-gray-400"
+              value={value}
+            >
               {name}
-            </span>
+            </data>
             <div
               className={cx(
-                'w-full overflow-hidden flex items-center',
+                'w-[80%] overflow-hidden flex items-center',
                 'cursor-pointer transition duration-100 hover:scale-y-125',
               )}
             >
               <div
-                className="rounded h-2 relative"
+                className={cx('rounded h-2 relative', color[name.toLowerCase()])}
                 style={{
-                  backgroundColor:
-                    mode === 'dark'
-                      ? colorDark[name.toLowerCase()]
-                      : color[name.toLowerCase()],
-                  width: `${barWidth}%`,
+                  width: `${(100 / maxValue) * value}%`,
                 }}
               ></div>
-              <div className="ml-2 right-0 top-0 text-xs text-gray-500 dark:text-gray-400">
+              <data className="ml-2 right-0 top-0 text-xs text-gray-500 dark:text-gray-400">
                 {value}
-              </div>
+              </data>
             </div>
           </div>
         );
@@ -215,65 +212,66 @@ const ChartSection = ({ values }: { values: ResultType[] }) => {
   );
 };
 
-export const ComplianceScanResult = ({ scanData }: { scanData: ScanResultType }) => {
+export const Scan = ({ scanData }: { scanData: ScanDataType }) => {
   const { accountId, data = [] } = scanData;
 
   return (
     <Card>
-      <summary className="grid grid-cols-[250px_1fr] items-center">
-        <AccountSection accountId={accountId} />
+      <div className="grid grid-cols-[250px_1fr] items-center">
+        <AccountComponent accountId={accountId} />
         <div className="flex flex-col">
-          {data.map(({ type, values, percentage }: ScanDataType, index: number) => {
+          {data.map(({ type, values, percentage }: ScanType, index: number) => {
             return (
               <div key={type}>
                 {index > 0 && index < data.length ? (
-                  <Separator className="mx-12 h-[1px] bg-gray-100 dark:bg-gray-700" />
+                  <Separator className="mx-6 h-[1px] bg-gray-100 dark:bg-gray-700" />
                 ) : null}
-                <div className="flex flex-col gap-3 p-3">
-                  <div className="p-3 grid grid-cols-[1fr_1fr]">
-                    <PercentAndTypSection type={type} percentage={percentage} />
-                    <ChartSection values={values} />
+                <div className="flex flex-col p-4">
+                  <div className="grid grid-cols-[1fr_1fr]">
+                    <TypeAndPercentageComponent type={type} percentage={percentage} />
+                    <ChartComponent values={values} />
                   </div>
                 </div>
               </div>
             );
           })}
         </div>
-      </summary>
+      </div>
     </Card>
   );
 };
 
 export const ComplianceScanSummary = () => {
   const [scanData] = useState(data);
-  const [scanning, setScanning] = useState(true);
+  const [scanning] = useState(false);
 
-  const headerText = scanning
-    ? 'Compliance Scan'
-    : 'View Compliance Scan Summary Results';
+  const headerText = scanning ? 'Compliance Scan' : 'Compliance Scan Results Summary';
 
   const subHeaderText = scanning
     ? 'A scan has been initiated, soon you will see your scan result.'
-    : 'Following are few scan result, for details information, go to dashboard';
+    : 'Summary of compliance scan result';
+
   return (
     <div className="flex flex-col">
       <ConnectorHeader title={headerText} description={subHeaderText} />
 
       {scanning ? (
-        <ScanInProgress />
+        <ScanInProgress text="Your Compliance Scan is currently running..." />
       ) : (
         <>
-          <Button size="sm" color="primary" className="ml-auto">
-            Go To Main Dashboard
-          </Button>
+          <Link
+            to="/"
+            className={cx(
+              `${Typography.size.sm} `,
+              'underline underline-offset-2 ml-auto bg-transparent text-blue-600 dark:text-blue-500',
+            )}
+          >
+            Go to Posture Dashboard to view details scan result
+          </Link>
+
           <div className="flex flex-col gap-4 mt-4">
-            {scanData.map((accountScanData: ScanResultType) => {
-              return (
-                <ComplianceScanResult
-                  key={accountScanData.accountId}
-                  scanData={accountScanData}
-                />
-              );
+            {scanData.map((accountScanData: ScanDataType) => {
+              return <Scan key={accountScanData.accountId} scanData={accountScanData} />;
             })}
           </div>
         </>
