11b3021ee71ece513aed8280555477092bb9c317
Saurabh Kumar <saurabh@deepfence.io>
2023-02-01 10:54:54 +0530

    Revert commit 26a7edd24b0555314d68b2501bb9e62554da9f5a

--- build.sh
+++ build.sh
@@ -27,14 +27,14 @@ else
     echo "SSL certificate found"
 fi
 
-# echo "Building init container image"
-# cd $DEEPFENCE_CONSOLE_DIR/init-container
-# docker build -f $DEEPFENCE_CONSOLE_DIR/init-container/Dockerfile -t ${IMAGE_REPOSITORY:-deepfenceio}/deepfence_init_ce:${DF_IMG_TAG:-latest} .
+echo "Building init container image"
+cd $DEEPFENCE_CONSOLE_DIR/init-container
+docker build -f $DEEPFENCE_CONSOLE_DIR/init-container/Dockerfile -t ${IMAGE_REPOSITORY:-deepfenceio}/deepfence_init_ce:${DF_IMG_TAG:-latest} .
 
-# if [ ! $? -eq 0 ]; then
-#     echo "Building init container image failed. Exiting"
-#     exit 1
-# fi
+if [ ! $? -eq 0 ]; then
+   echo "Building init container image failed. Exiting"
+   exit 1
+fi
 
 echo "Building Vulnerability mapper image"
 cd $DEEPFENCE_CONSOLE_DIR
@@ -45,107 +45,107 @@ if [ ! $? -eq 0 ]; then
     exit 1
 fi
 
-# echo "Creating elastic-search docker image. You can check $DOCKER_BUILD_LOG for status"
-# docker build --network host --tag=${IMAGE_REPOSITORY:-deepfenceio}/deepfence_elastic_ce:${DF_IMG_TAG:-latest} --rm=true -f elastic-Dockerfile .
+echo "Creating elastic-search docker image. You can check $DOCKER_BUILD_LOG for status"
+docker build --network host --tag=${IMAGE_REPOSITORY:-deepfenceio}/deepfence_elastic_ce:${DF_IMG_TAG:-latest} --rm=true -f elastic-Dockerfile .
 
-# if [ ! $? -eq 0 ]; then
-#     echo "Error while creating elastic-search docker. Check $DOCKER_BUILD_LOG"
-#     exit 1
-# fi
+if [ ! $? -eq 0 ]; then
+   echo "Error while creating elastic-search docker. Check $DOCKER_BUILD_LOG"
+   exit 1
+fi
 
 
-# echo "Creating redis docker image. You can check $DOCKER_BUILD_LOG for status"
-# docker build --network host --tag=${IMAGE_REPOSITORY:-deepfenceio}/deepfence_redis_ce:${DF_IMG_TAG:-latest} --rm=true -f redis-Dockerfile .
+echo "Creating redis docker image. You can check $DOCKER_BUILD_LOG for status"
+docker build --network host --tag=${IMAGE_REPOSITORY:-deepfenceio}/deepfence_redis_ce:${DF_IMG_TAG:-latest} --rm=true -f redis-Dockerfile .
 
-# if [ ! $? -eq 0 ]; then
-#     echo "Error while creating redis docker. Check $DOCKER_BUILD_LOG"
-#     exit 1
-# fi
+if [ ! $? -eq 0 ]; then
+   echo "Error while creating redis docker. Check $DOCKER_BUILD_LOG"
+   exit 1
+fi
 
-# echo "Building postgres"
-# docker build --network host --tag=${IMAGE_REPOSITORY:-deepfenceio}/deepfence_postgres_ce:${DF_IMG_TAG:-latest} --rm=true -f postgres-Dockerfile .
+echo "Building postgres"
+docker build --network host --tag=${IMAGE_REPOSITORY:-deepfenceio}/deepfence_postgres_ce:${DF_IMG_TAG:-latest} --rm=true -f postgres-Dockerfile .
 
-# if [ ! $? -eq 0 ]; then
-#     echo "Building postgres failed. Exiting"
-#     exit 1
-# fi
+if [ ! $? -eq 0 ]; then
+   echo "Building postgres failed. Exiting"
+   exit 1
+fi
 
-# echo "Building kafka broker"
-# docker build --network host --tag=${IMAGE_REPOSITORY:-deepfenceio}/deepfence_kafka_broker_ce:${DF_IMG_TAG:-latest} --rm=true -f kafka-broker-Dockerfile .
+echo "Building kafka broker"
+docker build --network host --tag=${IMAGE_REPOSITORY:-deepfenceio}/deepfence_kafka_broker_ce:${DF_IMG_TAG:-latest} --rm=true -f kafka-broker-Dockerfile .
 
-# if [ ! $? -eq 0 ]; then
-#     echo "Building postgres failed. Exiting"
-#     exit 1
-# fi
+if [ ! $? -eq 0 ]; then
+   echo "Building postgres failed. Exiting"
+   exit 1
+fi
 
-# echo "Building kafka rest proxy"
-# docker build --network host --tag=${IMAGE_REPOSITORY:-deepfenceio}/deepfence_kafka_rest_proxy_ce:${DF_IMG_TAG:-latest} --rm=true -f kafka-rest-proxy-Dockerfile .
+echo "Building kafka rest proxy"
+docker build --network host --tag=${IMAGE_REPOSITORY:-deepfenceio}/deepfence_kafka_rest_proxy_ce:${DF_IMG_TAG:-latest} --rm=true -f kafka-rest-proxy-Dockerfile .
 
-# if [ ! $? -eq 0 ]; then
-#     echo "Building postgres failed. Exiting"
-#     exit 1
-# fi
+if [ ! $? -eq 0 ]; then
+   echo "Building postgres failed. Exiting"
+   exit 1
+fi
 
-# echo "Building deepfence_router image"
-# docker build -f $DEEPFENCE_BACKEND_DIR/dockerify/haproxy/Dockerfile --build-arg is_dev_build=${IS_DEV_BUILD:-false} -t ${IMAGE_REPOSITORY:-deepfenceio}/deepfence_router_ce:${DF_IMG_TAG:-latest} $DEEPFENCE_BACKEND_DIR
+echo "Building deepfence_router image"
+docker build -f $DEEPFENCE_BACKEND_DIR/dockerify/haproxy/Dockerfile --build-arg is_dev_build=${IS_DEV_BUILD:-false} -t ${IMAGE_REPOSITORY:-deepfenceio}/deepfence_router_ce:${DF_IMG_TAG:-latest} $DEEPFENCE_BACKEND_DIR
 
-# if [ ! $? -eq 0 ]; then
-#     echo "Building deepfence_router failed. Exiting"
-#     exit 1
-# fi
+if [ ! $? -eq 0 ]; then
+   echo "Building deepfence_router failed. Exiting"
+   exit 1
+fi
 
-# echo "Building API image"
-# docker build -f $DEEPFENCE_BACKEND_DIR/dockerify/api/Dockerfile -t ${IMAGE_REPOSITORY:-deepfenceio}/deepfence_api_ce:${DF_IMG_TAG:-latest} $DEEPFENCE_BACKEND_DIR
+echo "Building API image"
+docker build -f $DEEPFENCE_BACKEND_DIR/dockerify/api/Dockerfile -t ${IMAGE_REPOSITORY:-deepfenceio}/deepfence_api_ce:${DF_IMG_TAG:-latest} $DEEPFENCE_BACKEND_DIR
 
-# if [ ! $? -eq 0 ]; then
-#     echo "Building API image failed. Exiting"
-#     exit 1
-# fi
+if [ ! $? -eq 0 ]; then
+   echo "Building API image failed. Exiting"
+   exit 1
+fi
 
-# echo "Building UI image"
-# git log --format="%h" -n 1 > $DEEPFENCE_UI_DIR/console_version.txt
-# echo "1.4.2" > $DEEPFENCE_UI_DIR/product_version.txt
-# docker build -f $DEEPFENCE_UI_DIR/Dockerfile -t ${IMAGE_REPOSITORY:-deepfenceio}/deepfence_ui_ce:${DF_IMG_TAG:-latest} $DEEPFENCE_UI_DIR
+echo "Building UI image"
+git log --format="%h" -n 1 > $DEEPFENCE_UI_DIR/console_version.txt
+echo "1.4.2" > $DEEPFENCE_UI_DIR/product_version.txt
+docker build -f $DEEPFENCE_UI_DIR/Dockerfile -t ${IMAGE_REPOSITORY:-deepfenceio}/deepfence_ui_ce:${DF_IMG_TAG:-latest} $DEEPFENCE_UI_DIR
 
-# if [ ! $? -eq 0 ]; then
-#     echo "Building UI image failed. Exiting"
-#     exit 1
-# fi
-# rm -rf $DEEPFENCE_UI_DIR/console_version.txt $DEEPFENCE_UI_DIR/product_version.txt
+if [ ! $? -eq 0 ]; then
+   echo "Building UI image failed. Exiting"
+   exit 1
+fi
+rm -rf $DEEPFENCE_UI_DIR/console_version.txt $DEEPFENCE_UI_DIR/product_version.txt
 
-# echo "Building fetcher"
-# docker build -f $DEEPFENCE_FETCHER_DIR/Dockerfile -t ${IMAGE_REPOSITORY:-deepfenceio}/deepfence_fetcher_ce:${DF_IMG_TAG:-latest} $DEEPFENCE_FETCHER_DIR
+echo "Building fetcher"
+docker build -f $DEEPFENCE_FETCHER_DIR/Dockerfile -t ${IMAGE_REPOSITORY:-deepfenceio}/deepfence_fetcher_ce:${DF_IMG_TAG:-latest} $DEEPFENCE_FETCHER_DIR
 
-# if [ ! $? -eq 0 ]; then
-#     echo "Building fetcher image failed. Exiting"
-#     exit 1
-# fi
+if [ ! $? -eq 0 ]; then
+   echo "Building fetcher image failed. Exiting"
+   exit 1
+fi
 
-# echo "Building diagnosis"
-# docker build -f $DEEPFENCE_DIAG_DIR/service/Dockerfile -t ${IMAGE_REPOSITORY:-deepfenceio}/deepfence_diagnosis_ce:${DF_IMG_TAG:-latest} $DEEPFENCE_DIAG_DIR/service
+echo "Building diagnosis"
+docker build -f $DEEPFENCE_DIAG_DIR/service/Dockerfile -t ${IMAGE_REPOSITORY:-deepfenceio}/deepfence_diagnosis_ce:${DF_IMG_TAG:-latest} $DEEPFENCE_DIAG_DIR/service
 
-# if [ ! $? -eq 0 ]; then
-#     echo "Building diagnosis image failed. Exiting"
-#     exit 1
-# fi
+if [ ! $? -eq 0 ]; then
+   echo "Building diagnosis image failed. Exiting"
+   exit 1
+fi
 
-# echo "Building Secret Scanner Image"
-# cd $SECRET_SCANNER_DIR
-# bash bootstrap.sh
-# docker build --rm=true --tag=${IMAGE_REPOSITORY:-deepfenceio}/deepfence_secret_scanner_ce:${DF_IMG_TAG:-latest} -f $SECRET_SCANNER_DIR/Dockerfile $SECRET_SCANNER_DIR
-# if [ ! $? -eq 0 ]; then
-#     echo "Building secret scanner image failed. Exiting"
-#     exit 1
-# fi
+echo "Building Secret Scanner Image"
+cd $SECRET_SCANNER_DIR
+bash bootstrap.sh
+docker build --rm=true --tag=${IMAGE_REPOSITORY:-deepfenceio}/deepfence_secret_scanner_ce:${DF_IMG_TAG:-latest} -f $SECRET_SCANNER_DIR/Dockerfile $SECRET_SCANNER_DIR
+if [ ! $? -eq 0 ]; then
+   echo "Building secret scanner image failed. Exiting"
+   exit 1
+fi
 
-# echo "Building Malware Scanner Image"
-# cd $MALWARE_SCANNER_DIR
-# bash bootstrap.sh
-# docker build --rm=true --tag=${IMAGE_REPOSITORY:-deepfenceio}/deepfence_malware_scanner_ce:${DF_IMG_TAG:-latest} -f $MALWARE_SCANNER_DIR/Dockerfile $MALWARE_SCANNER_DIR
-# if [ ! $? -eq 0 ]; then
-#     echo "Building malware scanner image failed. Exiting"
-#     exit 1
-# fi
+echo "Building Malware Scanner Image"
+cd $MALWARE_SCANNER_DIR
+bash bootstrap.sh
+docker build --rm=true --tag=${IMAGE_REPOSITORY:-deepfenceio}/deepfence_malware_scanner_ce:${DF_IMG_TAG:-latest} -f $MALWARE_SCANNER_DIR/Dockerfile $MALWARE_SCANNER_DIR
+if [ ! $? -eq 0 ]; then
+   echo "Building malware scanner image failed. Exiting"
+   exit 1
+fi
 
 echo "Building Package Scanner Image"
 cd $PACKAGE_SCANNER_DIR
