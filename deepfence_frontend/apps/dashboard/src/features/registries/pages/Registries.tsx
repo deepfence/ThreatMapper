457badeff167be443dff75f0dc736989b18a350c
milan-deepfence <milan@deepfence.io>
2023-02-01 21:28:29 +0530

    adjust alignment and remove not required tags

--- deepfence_frontend/apps/dashboard/src/features/registries/pages/Registries.tsx
+++ deepfence_frontend/apps/dashboard/src/features/registries/pages/Registries.tsx
@@ -12,7 +12,7 @@ export const Registries = () => {
         return (
           <Card className="p-4 flex flex-col items-center gap-2 shrink-0" key={i}>
             <div className="flex items-center justify-between w-full">
-              <h4 className="text-gray-900 text-xl dark:text-white">
+              <h4 className="text-gray-900 text-md dark:text-white">
                 Azure Container Registry
               </h4>
               <div className="flex ml-auto mt-2">
@@ -33,67 +33,60 @@ export const Registries = () => {
               </div>
             </div>
             <div className="flex items-center gap-6">
-              <div className="px-4 flex items-center border-r-2 border-gray-200 dark:border-gray-700 w-32 h-32">
+              <div className="px-4 flex items-center border-r border-gray-200 dark:border-gray-700 w-24 h-24">
                 <img height="100%" width="100%" src={LogoAzure} alt="logo" />
               </div>
-              <div>
-                <div className="flex flex-col mt-2">
-                  <div className="mt-2 flex gap-x-2 justify-center items-center">
-                    <div className="gap-x-2  flex justify-center items-center">
-                      <IconContext.Provider
-                        value={{
-                          className: 'h-6 w-6 text-blue-500 dark:text-blue-400',
-                        }}
-                      >
-                        <FaCloud />
-                      </IconContext.Provider>
-                      <div className="pr-4 flex items-center">
-                        <span className="text-[2.5rem] text-gray-900 dark:text-gray-200 font-light">
-                          23
-                        </span>
-                        <div className="pl-1 text-xs text-gray-400">
-                          <div>Total</div>
-                          <div>Accounts</div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="gap-x-2 flex justify-center items-center">
-                      <IconContext.Provider
-                        value={{
-                          className: 'h-6 w-6 text-teal-500 dark:text-teal-400',
-                        }}
-                      >
-                        <FaImages />
-                      </IconContext.Provider>
-                      <div className="pr-4 flex items-center">
-                        <span className="text-[2.5rem] text-gray-900 dark:text-gray-200 font-light">
-                          23
-                        </span>
-                        <span className="pl-1 text-xs text-gray-400">
-                          <div>Total</div>
-                          <div>Images</div>
-                        </span>
-                      </div>
-                    </div>
-                    <div className="gap-x-2 flex justify-center items-center">
-                      <IconContext.Provider
-                        value={{
-                          className: 'h-6 w-6 text-indigo-600 dark:text-indigo-400',
-                        }}
-                      >
-                        <FaTags />
-                      </IconContext.Provider>
-                      <div className="pr-4 flex items-center">
-                        <span className="text-[2.5rem] text-gray-900 dark:text-gray-200 font-light">
-                          23
-                        </span>
-                        <span className="pl-1 text-xs text-gray-400">
-                          <div>Total</div>
-                          <div>Tags</div>
-                        </span>
-                      </div>
-                    </div>
+              <div className="flex gap-x-4 justify-center items-center">
+                <div className="flex flex-col justify-center">
+                  <div className="pr-4 flex items-center gap-x-2">
+                    <IconContext.Provider
+                      value={{
+                        className: 'h-6 w-6 text-blue-500 dark:text-blue-400',
+                      }}
+                    >
+                      <FaCloud />
+                    </IconContext.Provider>
+                    <span className="text-[2rem] text-gray-900 dark:text-gray-200 font-light">
+                      23
+                    </span>
                   </div>
+                  <span className="text-xs text-gray-400 dark:text-gray-500">
+                    Total Accounts
+                  </span>
+                </div>
+                <div className="gap-x-2 flex flex-col justify-center">
+                  <div className="pr-4 flex items-center gap-x-2">
+                    <IconContext.Provider
+                      value={{
+                        className: 'h-6 w-6 text-teal-500 dark:text-teal-400',
+                      }}
+                    >
+                      <FaImages />
+                    </IconContext.Provider>
+                    <span className="text-[2rem] text-gray-900 dark:text-gray-200 font-light">
+                      23
+                    </span>
+                  </div>
+                  <span className="text-xs text-gray-400 dark:text-gray-500">
+                    Total Images
+                  </span>
+                </div>
+                <div className="gap-x-2 flex flex-col justify-center">
+                  <div className="pr-4 flex items-center gap-x-2">
+                    <IconContext.Provider
+                      value={{
+                        className: 'h-6 w-6 text-indigo-600 dark:text-indigo-400',
+                      }}
+                    >
+                      <FaTags />
+                    </IconContext.Provider>
+                    <span className="text-[2rem] text-gray-900 dark:text-gray-200 font-light">
+                      23
+                    </span>
+                  </div>
+                  <span className="text-xs text-gray-400 dark:text-gray-500">
+                    Total Tags
+                  </span>
                 </div>
               </div>
             </div>
