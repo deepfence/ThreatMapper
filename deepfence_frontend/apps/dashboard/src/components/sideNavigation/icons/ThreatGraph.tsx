a665fff091278a82ad7778cd5e9c46f3d6a4d5ae
Manan Vaghasiya <manan@ironmanan.com>
2023-02-14 06:22:17 +0000

    add side navigation icons

---
+++ deepfence_frontend/apps/dashboard/src/components/sideNavigation/icons/ThreatGraph.tsx
@@ -0,0 +1,68 @@
+export const ThreatGraphIcon = () => {
+  return (
+    <svg
+      width="100%"
+      height="100%"
+      viewBox="0 0 64 64"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clipPath="url(#clip0_19_137)">
+        <path
+          d="M18 26C20.2091 26 22 24.2091 22 22C22 19.7909 20.2091 18 18 18C15.7909 18 14 19.7909 14 22C14 24.2091 15.7909 26 18 26Z"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M40 42C42.2091 42 44 40.2091 44 38C44 35.7909 42.2091 34 40 34C37.7909 34 36 35.7909 36 38C36 40.2091 37.7909 42 40 42Z"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M44 26C47.3137 26 50 23.3137 50 20C50 16.6863 47.3137 14 44 14C40.6863 14 38 16.6863 38 20C38 23.3137 40.6863 26 44 26Z"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M20 50C23.3137 50 26 47.3137 26 44C26 40.6863 23.3137 38 20 38C16.6863 38 14 40.6863 14 44C14 47.3137 16.6863 50 20 50Z"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M26 42L36 39"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M21 25L36.62 35.74"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M22 22L38 20"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_19_137">
+          <rect width="48" height="48" fill="white" transform="translate(8 8)" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
