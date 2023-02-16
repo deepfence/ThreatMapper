565dc417e6fb116165b2f8ba67c041bc41f0ac7d
gnmahanth <mahanth@deepfence.io>
2023-02-16 08:05:20 +0000

    wait for neo4j to start in server and worker

--- deepfence_worker/entrypoint.sh
+++ deepfence_worker/entrypoint.sh
@@ -4,9 +4,17 @@ set -e
 # update vulnerability databae
 /usr/local/bin/grype db update
 
+# wait for neo4j to start
+until nc -z ${DEEPFENCE_NEO4J_HOST} ${DEEPFENCE_NEO4J_BOLT_PORT};
+do 
+  echo "neo4j is unavailable - sleeping"
+  sleep 5; 
+done
+
 # wait for kafka connection
 until kcat -L -b ${DEEPFENCE_KAFKA_BROKERS};
 do 
+  echo "kafka is unavailable - sleeping"
   sleep 5
 done
 
