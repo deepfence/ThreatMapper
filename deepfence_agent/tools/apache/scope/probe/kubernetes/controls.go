993495597cc16873cfcb66ed4a4d3210b4261ba8
Saurabh Kumar <saurabh@deepfence.io>
2023-02-20 13:33:00 +0530

    pass scan id to kspm

--- deepfence_agent/tools/apache/scope/probe/kubernetes/controls.go
+++ deepfence_agent/tools/apache/scope/probe/kubernetes/controls.go
@@ -1,6 +1,7 @@
 package kubernetes
 
 import (
+	"fmt"
 	ctl "github.com/deepfence/golang_deepfence_sdk/utils/controls"
 	k8sscanner "github.com/deepfence/kubernetes-scanner/scanner/compliance"
 	k8sscannerutil "github.com/deepfence/kubernetes-scanner/util"
@@ -12,10 +13,10 @@ func StartComplianceScan(req ctl.StartComplianceScanRequest) error {
 	scanner, err := k8sscanner.NewComplianceScanner(
 		k8sscannerutil.Config{
 			ComplianceCheckType:       k8sscannerutil.NsaCisaCheckType,
-			ScanId:                    "",
+			ScanId:                    req.BinArgs["scan_id"],
 			NodeId:                    req.NodeId,
-			NodeName:                  "",
-			ComplianceResultsFilePath: "/var/log/compliance/compliance-scan/<scan_id>.log",
+			NodeName:                  req.NodeId,
+			ComplianceResultsFilePath: fmt.Sprintf("/var/log/compliance/compliance-scan/%s.log", req.BinArgs["scan_id"]),
 			ComplianceStatusFilePath:  "/var/log/compliance/compliance-status/status.log",
 		})
 	if err != nil {
