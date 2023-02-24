ff6ad17fc74d44f49072d5864ba91ebf84ff7ebc
Saurabh Kumar <saurabh@deepfence.io>
2023-02-24 22:29:55 +0530

    cluster agent entrypoint fix

--- deepfence_agent/tools/apache/deepfence/start_cluster_agent.sh
+++
@@ -1,4 +0,0 @@
-envsubst '${SCOPE_HOSTNAME}:${MGMT_CONSOLE_URL}:${MGMT_CONSOLE_PORT}' </home/deepfence/supervisord-temp.conf >/home/deepfence/supervisord.conf
-unlink /var/run/supervisor.sock 2>/dev/null
-/usr/bin/supervisord -c /home/deepfence/supervisord.conf
-/home/deepfence/deepfence_exe
