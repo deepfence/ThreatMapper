38c64cf05bdc40fa1d4bcafb328ee825d66448f3
gnmahanth <mahanth@deepfence.io>
2023-03-02 15:38:36 +0000

    add audit logs cleanup to db cleanup task

--- deepfence_worker/cronjobs/postgresql.go
+++ deepfence_worker/cronjobs/postgresql.go
@@ -1,9 +1,11 @@
 package cronjobs
 
 import (
+	"time"
+
 	"github.com/ThreeDotsLabs/watermill/message"
 	"github.com/deepfence/golang_deepfence_sdk/utils/directory"
-	"time"
+	"github.com/deepfence/golang_deepfence_sdk/utils/log"
 )
 
 // CleanUpPostgresDB Delete expired user invites and password reset requests
@@ -18,18 +20,27 @@ func CleanUpPostgresDB(msg *message.Message) error {
 	dateTime := string(msg.Payload)
 	ts, err := time.Parse("2006-01-02T15:04:05.999Z", dateTime)
 	if err != nil {
-		return err
+		log.Error().Msg(err.Error())
+		return nil
 	}
 
 	err = pgClient.DeletePasswordResetByExpiry(ctx, ts)
 	if err != nil {
-		return err
+		log.Error().Msg(err.Error())
 	}
 
 	err = pgClient.DeleteUserInviteByExpiry(ctx, ts)
 	if err != nil {
-		return err
+		log.Error().Msg(err.Error())
 	}
+
+	// delete audit logs older than 30days
+	deleted, err := pgClient.DeleteAuditLogsOlderThan30days(ctx)
+	if err != nil {
+		log.Error().Msg(err.Error())
+	}
+
+	log.Info().Msgf("deleted %d audit logs which were older than 30days", deleted)
 
 	return nil
 }
