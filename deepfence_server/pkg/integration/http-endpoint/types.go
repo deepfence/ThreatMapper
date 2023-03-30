58a6a42a508243da1bd284491fbe02767dc1a141
Harshvardhan Karn <harshvardhan@deepfence.io>
2023-03-30 06:09:55 +0000

    add more integrations

---
+++ deepfence_server/pkg/integration/http-endpoint/types.go
@@ -0,0 +1,14 @@
+package httpendpoint
+
+type HTTPEndpoint struct {
+	Config           Config              `json:"config"`
+	IntegrationType  string              `json:"integration_type"`
+	NotificationType string              `json:"notification_type"`
+	Filters          map[string][]string `json:"filters"`
+	Message          string              `json:"message"`
+}
+
+type Config struct {
+	URL     string `json:"url"`
+	AuthKey string `json:"auth_key"`
+}
