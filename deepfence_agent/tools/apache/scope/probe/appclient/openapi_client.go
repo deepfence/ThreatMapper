4eebb9940c08d6c463c5bebadd2041d7e50581e8
Thomas Legris <thomas@deepfence.io>
2023-05-14 18:59:39 +0900

    Keep using same HTTP client

--- deepfence_agent/tools/apache/scope/probe/appclient/openapi_client.go
+++ deepfence_agent/tools/apache/scope/probe/appclient/openapi_client.go
@@ -1,10 +1,14 @@
 package appclient
 
 import (
+	"bytes"
+	"compress/gzip"
+	"net/http"
 	"net/url"
 	"os"
 	"sync/atomic"
 
+	"github.com/bytedance/sonic"
 	openapi "github.com/deepfence/golang_deepfence_sdk/client"
 	"github.com/weaveworks/scope/probe/common"
 	"github.com/weaveworks/scope/report"
@@ -14,7 +18,7 @@ type OpenapiClient struct {
 	client               *openapi.APIClient
 	stopControlListening chan struct{}
 	publishInterval      atomic.Int32
-	httpClient           *Client
+	publishReportUrl     string
 }
 
 func NewOpenapiClient() (*OpenapiClient, error) {
@@ -22,19 +26,11 @@ func NewOpenapiClient() (*OpenapiClient, error) {
 	if err != nil {
 		return nil, err
 	}
-	httpClient, err := NewClient(
-		os.Getenv("MGMT_CONSOLE_URL"),
-		os.Getenv("MGMT_CONSOLE_PORT"),
-		os.Getenv("DEEPFENCE_KEY"),
-	)
-	if err != nil {
-		return nil, err
-	}
 	res := &OpenapiClient{
 		client:               openapiClient,
 		stopControlListening: make(chan struct{}),
 		publishInterval:      atomic.Int32{},
-		httpClient:           httpClient,
+		publishReportUrl:     "https://"+os.Getenv("MGMT_CONSOLE_URL") + "/deepfence/ingest/report",
 	}
 	res.publishInterval.Store(10)
 
@@ -43,7 +39,37 @@ func NewOpenapiClient() (*OpenapiClient, error) {
 
 // Publish implements MultiAppClient
 func (ct *OpenapiClient) Publish(r report.Report) error {
-	_, err := ct.httpClient.PublishAgentReport(r)
+	c := ct.client.GetConfig().HTTPClient
+	buf, err := sonic.Marshal(r)
+	if err != nil {
+		return err
+	}
+	var b bytes.Buffer
+	gz := gzip.NewWriter(&b)
+	if _, err := gz.Write(buf); err != nil {
+		return err
+	}
+	err = gz.Close()
+	if err != nil {
+		return err
+	}
+
+	httpReq, err := http.NewRequest(http.MethodPost, ct.publishReportUrl, bytes.NewReader(b.Bytes()))
+	if err != nil {
+		return err
+	}
+	httpReq.Close = true
+
+	for k, v := range ct.client.GetConfig().DefaultHeader {
+		httpReq.Header.Add(k, v)
+	}
+	httpReq.Header.Add("Content-Encoding", "gzip")
+
+	resp, err := c.Do(httpReq)
+	if err != nil {
+		return err
+	}
+	resp.Body.Close()
 	return err
 }
 
