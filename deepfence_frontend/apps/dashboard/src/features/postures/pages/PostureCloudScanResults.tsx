eff5599eac2aaa883deb18ca26f94a24660529d5
harishshg <sharish98@gmail.com>
2023-05-16 00:13:41 +0530

    Select All Checkbox state is not resetting after deletion (Secret , Malware , Vulnerability and postures)

--- deepfence_frontend/apps/dashboard/src/features/postures/pages/PostureCloudScanResults.tsx
+++ deepfence_frontend/apps/dashboard/src/features/postures/pages/PostureCloudScanResults.tsx
@@ -1,6 +1,6 @@
 import cx from 'classnames';
 import { capitalize } from 'lodash-es';
-import { Suspense, useCallback, useMemo, useRef, useState } from 'react';
+import { Suspense, useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { FaHistory } from 'react-icons/fa';
 import { FiFilter } from 'react-icons/fi';
 import {
@@ -823,6 +823,11 @@ const ScanResultTable = () => {
   const [sort, setSort] = useSortingState();
   const [showDeleteDialog, setShowDeleteDialog] = useState(false);
   const [idsToDelete, setIdsToDelete] = useState<string[]>([]);
+  useEffect(() => {
+    if (idsToDelete.length) {
+      setRowSelectionState({});
+    }
+  }, [loaderData.data]);
 
   const columns = useMemo(() => {
     const columns = [
