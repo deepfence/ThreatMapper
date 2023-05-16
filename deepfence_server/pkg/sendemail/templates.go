6e58aebed05942197a04695fda8c06c82665fcd6
Ramanan Ravikumar <ramanan@deepfence.io>
2023-05-16 20:59:20 +0530

    Send email for user invite, password reset #725

--- deepfence_server/pkg/sendemail/templates.go
+++ deepfence_server/pkg/sendemail/templates.go
@@ -1,7 +1,7 @@
 package sendemail
 
 var (
-	PasswordResetEmail = "Hello %s %s, \nPlease click here to reset your Deepfence ThreatMapper password: %s (Link valid for %d minutes)"
+	PasswordResetEmail = "Hello %s %s, \n\nPlease click here to reset your Deepfence ThreatMapper password: %s (Link valid for %d minutes)\n"
 
-	UserInviteEmail = "Hello, \nYou have been invited to join Deepfence ThreatMapper by %s %s (%s). \nPlease click here to register: %s"
+	UserInviteEmail = "Hello, \n\nYou have been invited to join Deepfence ThreatMapper by %s %s (%s). \nPlease click here to register: %s\n"
 )
