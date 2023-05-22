e4a8fb154ce3f1660bbfccce994f02e124892921
gnmahanth <mahanth@deepfence.io>
2023-05-22 06:01:21 +0000

    fix: worker build failure with missing proto files

--- deepfence_agent/build/grpccode-build.sh
+++ deepfence_agent/build/grpccode-build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-cp -R plugins /tmp
-cd /tmp/plugins
-make clean
-make proto -B
+cp -R plugins /tmp && cd /tmp/plugins && make clean && make proto -B
 build_result=$?
 if [ $build_result -ne 0 ]
 then
@@ -12,4 +9,6 @@ fi
 cd -
 rm -rf ./tools/apache/scope/proto
 mkdir ./tools/apache/scope/proto 2>/dev/null
-cp /tmp/plugins/proto/* ./tools/apache/scope/proto
+cp /tmp/plugins/proto/*.go ./tools/apache/scope/proto
+cp /tmp/plugins/proto/*.go ./plugins/agent-plugins-grpc/proto
+mkdir -p ./plugins/proto && cp /tmp/plugins/proto/*.proto ./plugins/proto
