e8eea4405bfef8319c38717d43ccb73fb7bb5cfe
harishshg <sharish98@gmail.com>
2023-05-23 16:47:14 +0530

    added email integration

--- deepfence_frontend/apps/dashboard/src/features/integrations/pages/IntegrationAdd.tsx
+++ deepfence_frontend/apps/dashboard/src/features/integrations/pages/IntegrationAdd.tsx
@@ -81,6 +81,10 @@ const getConfigBodyNotificationType = (formData: FormData, integrationType: stri
         url: formBody.apiUrl,
         auth_key: formBody.authorizationKey,
       };
+    case IntegrationType.email:
+      return {
+        email: formBody.email,
+      };
     case IntegrationType.splunk:
       return {
         endpoint_url: formBody.url,
