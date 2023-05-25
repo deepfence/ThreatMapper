b20478e4f9602eb0a5d1e311b93b522919c58ff6
Harshvardhan Karn <harshvardhan@deepfence.io>
2023-05-25 04:57:52 +0000

    add support to scan registry at image level

--- deepfence_server/handler/scan_reports.go
+++ deepfence_server/handler/scan_reports.go
@@ -1526,11 +1526,22 @@ func FindNodesMatching(ctx context.Context,
 		return res, err
 	}
 	res = append(res, rh...)
-	ri, err := get_node_ids(tx, image_ids, controls.Image, filter.ImageScanFilter)
-	if err != nil {
-		return res, err
+
+	if len(filter.ImageScanFilter.FieldsValues["docker_image_name"]) > 0 &&
+		len(filter.ImageScanFilter.FieldsValues["docker_image_tag"]) > 0 {
+		ri, err := GetImagesFromAdvanceFilter(ctx, image_ids, filter.ImageScanFilter)
+		if err != nil {
+			return res, err
+		}
+		res = append(res, ri...)
+	} else {
+		ri, err := get_node_ids(tx, image_ids, controls.Image, filter.ImageScanFilter)
+		if err != nil {
+			return res, err
+		}
+		res = append(res, ri...)
 	}
-	res = append(res, ri...)
+
 	rc, err := get_node_ids(tx, container_ids, controls.Container, filter.ContainerScanFilter)
 	if err != nil {
 		return res, err
@@ -1547,6 +1558,92 @@ func FindNodesMatching(ctx context.Context,
 	}
 	res = append(res, rk...)
 
+	return res, nil
+}
+
+func GetImagesFromAdvanceFilter(ctx context.Context, ids []model.NodeIdentifier, filter reporters.ContainsFilter) ([]model.NodeIdentifier, error) {
+	res := []model.NodeIdentifier{}
+
+	driver, err := directory.Neo4jClient(ctx)
+
+	if err != nil {
+		return res, err
+	}
+
+	session := driver.NewSession(neo4j.SessionConfig{AccessMode: neo4j.AccessModeRead})
+
+	defer session.Close()
+
+	tx, err := session.BeginTransaction()
+	if err != nil {
+		return res, err
+	}
+	defer tx.Close()
+
+	for i := range filter.FieldsValues["docker_image_name"] {
+		rr, err := tx.Run(`
+		MATCH (n:ContainerImage)
+		WHERE n.node_id IN $ids
+		AND n.docker_image_name = $image_name
+		RETURN n.node_id, n.updated_at, n.docker_image_tag
+		`, map[string]interface{}{
+			"ids":        reporters_scan.NodeIdentifierToIdList(ids),
+			"image_name": filter.FieldsValues["docker_image_name"][i],
+		})
+		if err != nil {
+			return res, err
+		}
+
+		rec, err := rr.Collect()
+		if err != nil {
+			return res, err
+		}
+
+		log.Info().Msgf("[GetImagesFromAdvanceFilter] rec: %v", rec)
+
+		if len(rec) == 0 {
+			return res, nil
+		}
+
+		switch filter.FieldsValues["docker_image_tag"][0] {
+		case "latest":
+			for j := range rec {
+				if rec[j].Values[2].(string) == filter.FieldsValues["docker_image_tag"][0] {
+					res = append(res, model.NodeIdentifier{
+						NodeId:   rec[j].Values[0].(string),
+						NodeType: controls.ResourceTypeToString(controls.Image),
+					})
+					break
+				}
+			}
+		case "all":
+			for j := range rec {
+				res = append(res, model.NodeIdentifier{
+					NodeId:   rec[j].Values[0].(string),
+					NodeType: controls.ResourceTypeToString(controls.Image),
+				})
+			}
+		case "recent":
+			log.Info().Msg("[GetImagesFromAdvanceFilter] recent called")
+			recentNodeID := rec[0].Values[0].(string)
+			recentTimeUNIX := rec[0].Values[1].(int64)
+			log.Info().Msgf("[GetImagesFromAdvanceFilter] recentNodeID: %v, recentTimeUNIX: %v", recentNodeID, recentTimeUNIX)
+			for j := range rec {
+				recentTime := time.Unix(recentTimeUNIX, 0)
+				t := time.Unix(rec[j].Values[1].(int64), 0)
+				if t.After(recentTime) {
+					recentTimeUNIX = rec[j].Values[1].(int64)
+					recentNodeID = rec[j].Values[0].(string)
+				}
+				log.Info().Msgf("[GetImagesFromAdvanceFilter] recentTime %v, t %v, recentNodeID %v", recentTime, t, recentNodeID)
+			}
+			res = append(res, model.NodeIdentifier{
+				NodeId:   recentNodeID,
+				NodeType: controls.ResourceTypeToString(controls.Image),
+			})
+		}
+	}
+	log.Info().Msgf("[GetImagesFromAdvanceFilter] res: %v", res)
 	return res, nil
 }
 
@@ -1618,6 +1715,9 @@ func StartMultiScan(ctx context.Context,
 	req model.ScanTriggerCommon,
 	actionBuilder func(string, model.NodeIdentifier, int32) (ctl.Action, error)) ([]string, string, error) {
 
+	// log parameter
+	log.Info().Msgf("StartMultiScan: gen_bulk_id %v, scan_type %v, req %+v", gen_bulk_id, scan_type, req)
+
 	driver, err := directory.Neo4jClient(ctx)
 
 	if err != nil {
@@ -1637,25 +1737,35 @@ func StartMultiScan(ctx context.Context,
 
 	regular, k8s, registry := extractBulksNodes(req.NodeIds)
 
+	log.Info().Msgf("StartMultiScan: regular %+v, k8s %+v, registry %+v", regular, k8s, registry)
+
 	image_nodes, err := reporters_scan.GetRegistriesImageIDs(ctx, registry)
 	if err != nil {
 		return nil, "", err
 	}
+
+	log.Info().Msgf("StartMultiScan: image_nodes %+v", image_nodes)
 
 	k8s_host_nodes, err := reporters_scan.GetKubernetesHostsIDs(ctx, k8s)
 	if err != nil {
 		return nil, "", err
 	}
 
+	log.Info().Msgf("StartMultiScan: k8s_host_nodes %+v", k8s_host_nodes)
+
 	k8s_image_nodes, err := reporters_scan.GetKubernetesImageIDs(ctx, k8s)
 	if err != nil {
 		return nil, "", err
 	}
 
+	log.Info().Msgf("StartMultiScan: k8s_image_nodes %+v", k8s_image_nodes)
+
 	k8s_container_nodes, err := reporters_scan.GetKubernetesContainerIDs(ctx, k8s)
 	if err != nil {
 		return nil, "", err
 	}
+
+	log.Info().Msgf("StartMultiScan: k8s_container_nodes %+v", k8s_container_nodes)
 
 	reqs := regular
 	if len(k8s) != 0 || len(registry) != 0 {
@@ -1673,6 +1783,8 @@ func StartMultiScan(ctx context.Context,
 	} else {
 		reqs = req.NodeIds
 	}
+	log.Info().Msgf("-----------")
+	log.Info().Msgf("StartMultiScan: reqs %+v", reqs)
 
 	defer tx.Close()
 	scanIds := []string{}
@@ -1715,13 +1827,16 @@ func StartMultiScan(ctx context.Context,
 		scanIds = append(scanIds, scanId)
 	}
 
+	log.Info().Msgf("StartMultiScan: scanIds %+v", scanIds)
 	if len(scanIds) == 0 {
 		return []string{}, "", nil
 	}
 
+	log.Info().Msgf("StartMultiScan: gen_bulk_id %v, scan_type %v, scanIds %+v", gen_bulk_id, scan_type, scanIds)
 	var bulkId string
 	if gen_bulk_id {
 		bulkId = bulkScanId()
+		log.Info().Msgf("StartMultiScan: bulkId %s", bulkId)
 		err = ingesters.AddBulkScan(ingesters.WriteDBTransaction{Tx: tx}, scan_type, bulkId, scanIds)
 		if err != nil {
 			log.Error().Msgf("%v", err)
