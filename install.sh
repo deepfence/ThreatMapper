cf90b32d1c4adc6ac9a8f184514bb203630fe494
Harshvardhan Karn <harshvardhan@deepfence.io>
2023-05-29 09:08:23 +0000

    add script to install console on docker and k8s

---
+++ install.sh
@@ -0,0 +1,180 @@
+#!/bin/bash
+
+# Define color codes
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'  # No color
+
+# Checkmark and Cross symbols
+CHECKMARK="${GREEN}✔${NC}"
+CROSS="${RED}✘${NC}"
+
+# Default image tag
+DEFAULT_IMAGE_TAG="2.0.0"
+
+# Parse command line arguments
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        -d|--deployment)
+        DEPLOYMENT="$2"
+        shift
+        shift
+        ;;
+        -t|--image-tag)
+        IMAGE_TAG="$2"
+        shift
+        shift
+        ;;
+        *) # Unknown option
+        shift
+        ;;
+    esac
+done
+
+# Set image tag
+if [[ -n $IMAGE_TAG ]]; then
+    echo -e "${CHECKMARK} Using image tag: $IMAGE_TAG"
+elif [[ -n $DF_IMG_TAG ]]; then
+    IMAGE_TAG=$DF_IMG_TAG
+    echo -e "${CHECKMARK} Using image tag from DF_IMG_TAG environment variable: $IMAGE_TAG"
+else
+    IMAGE_TAG=$DEFAULT_IMAGE_TAG
+    echo -e "${CHECKMARK} Using default image tag: $IMAGE_TAG"
+fi
+
+# Check minimum system requirements for Docker
+check_docker_requirements() {
+    echo -e "${GREEN}Checking minimum system requirements for Docker...${NC}"
+
+    # Check CPU cores
+    MIN_CPU=4
+    ACTUAL_CPU=$(nproc)
+    if [[ $ACTUAL_CPU -lt $MIN_CPU ]]; then
+        echo -e "${CROSS} Insufficient CPU cores for Docker. Minimum requirement: $MIN_CPU cores.${NC}"
+        exit 1
+    fi
+    echo -e "${CHECKMARK} CPU cores check passed for Docker."
+
+    # Check RAM
+    MIN_RAM=16
+    ACTUAL_RAM=$(free -g | awk '/^Mem:/{print $2}')
+    if [[ $ACTUAL_RAM -lt $MIN_RAM ]]; then
+        echo -e "${CROSS} Insufficient RAM for Docker. Minimum requirement: $MIN_RAM GB.${NC}"
+        exit 1
+    fi
+    echo -e "${CHECKMARK} RAM check passed for Docker."
+}
+
+# Check Docker and Docker Compose
+check_docker() {
+    echo -e "${GREEN}Checking Docker and Docker Compose...${NC}"
+    if ! command -v docker &> /dev/null; then
+        echo -e "${CROSS} Docker not found. Please install Docker.${NC}"
+        exit 1
+    fi
+    echo -e "${CHECKMARK} Docker check passed."
+
+    if ! command -v docker-compose &> /dev/null; then
+        echo -e "${CROSS} Docker Compose not found. Please install Docker Compose.${NC}"
+        exit 1
+    fi
+    echo -e "${CHECKMARK} Docker Compose check passed."
+}
+
+# Check CPU and RAM for each Kubernetes node
+check_kubernetes_node_requirements() {
+    echo -e "${GREEN}Checking CPU and RAM for each Kubernetes node...${NC}"
+    MIN_CPU=4
+    MIN_RAM=8
+
+    # Get the number of nodes in the Kubernetes cluster
+    NODE_COUNT=$(kubectl get nodes --no-headers | wc -l)
+    if [[ $NODE_COUNT -lt 3 ]]; then
+        echo -e "${CROSS} Insufficient number of Kubernetes nodes. Minimum requirement: 3 nodes.${NC}"
+        exit 1
+    fi
+    echo -e "${CHECKMARK} Kubernetes nodes check passed."
+
+    # Check CPU and RAM for each node
+    NODES=$(kubectl get nodes --no-headers | awk '{print $1}')
+    while IFS= read -r node; do
+        CPU=$(kubectl describe node "$node" | grep "cpu: " | awk '{print $2}')
+        RAM=$(kubectl describe node "$node" | grep "memory: " | awk '{print $2}')
+
+        if [[ $CPU -lt $MIN_CPU || $RAM -lt $MIN_RAM ]]; then
+            echo -e "${CROSS} Insufficient CPU or RAM for Kubernetes node: $node. Minimum requirement: $MIN_CPU cores, $MIN_RAM GB RAM.${NC}"
+            exit 1
+        fi
+
+        echo -e "${CHECKMARK} CPU and RAM check passed for Kubernetes node: $node."
+    done <<< "$NODES"
+}
+
+# Check Kubernetes, Helm, and the cluster
+check_kubernetes() {
+    echo -e "${GREEN}Checking Kubernetes, Helm, and the cluster...${NC}"
+    if ! command -v kubectl &> /dev/null; then
+        echo -e "${CROSS} kubectl not found. Please install kubectl.${NC}"
+        exit 1
+    fi
+    echo -e "${CHECKMARK} kubectl check passed."
+
+    if ! command -v helm &> /dev/null; then
+        echo -e "${CROSS} Helm not found. Please install Helm.${NC}"
+        exit 1
+    fi
+    echo -e "${CHECKMARK} Helm check passed."
+
+    # Check cluster connectivity or any other cluster-specific checks
+    # Add your custom checks here
+
+    # Check CPU and RAM for each Kubernetes node
+    check_kubernetes_node_requirements
+}
+
+# Perform preflight checks based on the deployment option
+preflight_checks() {
+    case $DEPLOYMENT in
+        docker)
+        check_docker_requirements
+        check_docker
+        ;;
+        kubernetes)
+        check_kubernetes
+        ;;
+        *)
+        echo -e "${RED}Invalid deployment option. Please specify 'docker' or 'kubernetes'.${NC}"
+        exit 1
+        ;;
+    esac
+}
+
+# Deploy using Docker Compose
+docker_deployment() {
+    echo -e "${GREEN}Performing Docker deployment with image tag: $IMAGE_TAG${NC}"
+    docker-compose -f deployment-scripts/docker-compose.yml up -d
+}
+
+# Deploy using Kubernetes and Helm
+kubernetes_deployment() {
+    echo -e "${GREEN}Performing Kubernetes deployment with image tag: $IMAGE_TAG${NC}"
+    # Execute helm chart apply or any other necessary commands
+}
+
+# Main script flow
+preflight_checks
+
+case $DEPLOYMENT in
+    docker)
+    docker_deployment
+    ;;
+    kubernetes)
+    kubernetes_deployment
+    ;;
+    *)
+    # This should not happen since we check the option earlier, but adding for completeness
+    echo -e "${RED}Invalid deployment option. Please specify 'docker' or 'kubernetes'.${NC}"
+    exit 1
+    ;;
+esac
