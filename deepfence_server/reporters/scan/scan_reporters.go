3146d6e1cca927f95bf07a8857833b2bb8137051
Thomas Legris <thomas@deepfence.io>
2023-05-29 17:17:06 +0900

    Fix missing node_id in results

--- deepfence_server/reporters/scan/scan_reporters.go
+++ deepfence_server/reporters/scan/scan_reporters.go
@@ -571,7 +571,7 @@ func GetScanResults[T any](ctx context.Context, scan_type utils.Neo4jScanType, s
 	query = `
 		MATCH (m:` + string(scan_type) + `{node_id: $scan_id}) -[r:DETECTED]-> (d)
 		OPTIONAL MATCH (d) -[:IS]-> (e)
-	WITH apoc.map.merge(d{.*, masked: coalesce(d.masked or r.masked, false), name: coalesce(e.name, d.name, '')}, e{.*, node_id:NULL}) as d` +
+	WITH apoc.map.merge( e{.*}, d{.*, masked: coalesce(d.masked or r.masked, false), name: coalesce(e.name, d.name, '')}) as d` +
 		reporters.ParseFieldFilters2CypherWhereConditions("d", mo.Some(ff), true) +
 		reporters.OrderFilter2CypherCondition("d", ff.OrderFilter) +
 		` RETURN d ` +
