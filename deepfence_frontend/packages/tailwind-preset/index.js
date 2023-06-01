645b2b7679ef8a0cac57de252dd6bb73e450590b
Manan Vaghasiya <manan@ironmanan.com>
2023-06-01 13:57:40 +0000

    update font family

--- deepfence_frontend/packages/tailwind-preset/index.js
+++ deepfence_frontend/packages/tailwind-preset/index.js
@@ -4,7 +4,7 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: [
-          'Inter',
+          'Nunito',
           'ui-sans-serif',
           'system-ui',
           '-apple-system',
@@ -21,7 +21,7 @@ module.exports = {
           'Noto Color Emoji',
         ],
         body: [
-          'Inter',
+          'Nunito',
           'ui-sans-serif',
           'system-ui',
           '-apple-system',
