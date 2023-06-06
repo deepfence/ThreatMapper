3f03719eb5ecd552944135a4f45a71ac9052c103
milan-deepfence <rkmilansingh@gmail.com>
2023-06-06 19:48:55 +0530

    update tab style

--- deepfence_frontend/packages/ui-components/src/components/tabs/Tabs.tsx
+++ deepfence_frontend/packages/ui-components/src/components/tabs/Tabs.tsx
@@ -7,7 +7,7 @@ import { IconContext } from 'react-icons';
 
 import { ObjectWithNonNullableValues } from '@/types/utils';
 
-export type SizeType = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+export type SizeType = 'md';
 export type TabVariantProps = ObjectWithNonNullableValues<
   VariantProps<typeof tabListCva>
 >;
@@ -24,110 +24,49 @@ export type TabProps = TabsPrimitive.TabsProps & {
   variant?: keyof TabVariantProps;
 };
 
-const labelCva = cva('cursor-pointer font-medium', {
+const labelCva = cva('cursor-pointer uppercase');
+const tabListCva = cva(
+  ['inline-flex cursor-pointer', 'text-center text-gray-500 dark:text-[#ADBBC4]'],
+  {
+    variants: {
+      underline: {
+        true: ['border-b border-gray-200 dark:border-bg-grid-border'],
+      },
+    },
+    defaultVariants: {
+      underline: true,
+    },
+  },
+);
+
+const tabItemCva = cva(['outline-none text-t4 cursor-pointer'], {
   variants: {
     size: {
-      xs: 'text-xs',
-      sm: 'text-sm',
-      md: 'text-base',
-      lg: 'text-lg',
-      xl: 'text-xl',
+      md: '',
     },
+    underline: {
+      true: [
+        cx(
+          'pt-[15px] px-3 flex items-center',
+          'radix-state-active:pb-[6px] radix-state-inactive:pb-[9px] ',
+          'radix-state-active:border-b-[3px] radix-state-active:text-text-input-value radix-state-active:border-accent-accent',
+        ),
+      ],
+    },
+  },
+  defaultVariants: {
+    underline: true,
+    size: 'md',
   },
 });
-const tabListCva = cva(
-  ['inline-flex', 'text-sm font-medium text-center text-gray-500 dark:text-gray-400'],
-  {
-    variants: {
-      underline: {
-        true: ['gap-x-4 border-b', 'border-gray-200 dark:border-gray-700'],
-      },
-      tab: {
-        true: [
-          'shadow dark:bg-gray-700',
-          'w-fit overflow-hidden',
-          'divide-x divide-gray-200 rounded-lg',
-          'dark:divide-gray-600',
-        ],
-      },
-    },
-    defaultVariants: {
-      underline: true,
-    },
-  },
-);
-
-const tabItemCva = cva(
-  [
-    'outline-none',
-    'ring-offset-0 focus-visible:radix-state-active:ring-4',
-    'focus-visible:radix-state-active:ring-blue-200 dark:focus-visible:radix-state-active:ring-blue-800',
-  ],
-  {
-    variants: {
-      size: {
-        xs: '',
-        sm: '',
-        md: '',
-        lg: '',
-        xl: '',
-      },
-      underline: {
-        true: [
-          'pb-2 px-3',
-          'border-gray-200 dark:border-gray-700',
-          'radix-state-active:border-b radix-state-active:-mb-px radix-state-active:text-blue-600 radix-state-active:border-blue-600',
-        ],
-      },
-      tab: {
-        true: [
-          'inline-flex radix-state-active:text-gray-900 dark:radix-state-active:text-white',
-          'radix-state-active:bg-gray-100 dark:radix-state-active:bg-gray-600',
-        ],
-      },
-    },
-    compoundVariants: [
-      {
-        tab: true,
-        size: 'xs',
-        className: 'py-2 px-3',
-      },
-      {
-        tab: true,
-        size: 'sm',
-        className: 'py-2.5 px-4',
-      },
-      {
-        tab: true,
-        size: 'md',
-        className: 'py-3 px-5',
-      },
-      {
-        tab: true,
-        size: 'lg',
-        className: 'py-3.5 px-6',
-      },
-      {
-        tab: true,
-        size: 'xl',
-        className: 'py-4 px-7',
-      },
-    ],
-    defaultVariants: {
-      underline: true,
-      size: 'sm',
-    },
-  },
-);
 
 const Tabs = (props: TabProps) => {
-  const { tabs, value, size = 'sm', children, variant = 'underline', ...rest } = props;
+  const { tabs, value, size = 'md', children, variant = 'underline', ...rest } = props;
   return (
     <TabsPrimitive.Root {...rest} data-testid={'tabs-testid'} value={value}>
       <TabsPrimitive.List
         className={tabListCva({
           underline: variant === 'underline',
-          tab: variant === 'tab',
         })}
       >
         {tabs.map(({ label, value, id, icon }) => {
@@ -139,21 +78,15 @@ const Tabs = (props: TabProps) => {
               data-testid={`tab-item-${_id}`}
               className={tabItemCva({
                 underline: variant === 'underline',
-                tab: variant === 'tab',
                 size,
               })}
             >
               {icon && (
-                <IconContext.Provider value={{ className: cx('w-4 h-4 mr-2 inline') }}>
+                <IconContext.Provider value={{ className: cx('w-3 h-3 mr-1 inline') }}>
                   {icon}
                 </IconContext.Provider>
               )}
-              <LabelPrimitive.Label
-                htmlFor={_id}
-                className={labelCva({
-                  size,
-                })}
-              >
+              <LabelPrimitive.Label htmlFor={_id} className={labelCva()}>
                 {label}
               </LabelPrimitive.Label>
             </TabsPrimitive.Trigger>
