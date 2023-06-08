fdb46022043bdf0f85831d0476bcaff0bc5ba9a8
milan-deepfence <rkmilansingh@gmail.com>
2023-06-08 14:50:27 +0530

    add new modal style

--- deepfence_frontend/packages/ui-components/src/components/modal/Modal.stories.tsx
+++ deepfence_frontend/packages/ui-components/src/components/modal/Modal.stories.tsx
@@ -12,11 +12,43 @@ export default {
   },
 } as Meta<typeof Modal>;
 
-export const DefaultOpenModal = () => {
+export const DefaultOpenModalS = () => {
   const [, setOpen] = useState(false);
   return (
-    <Modal defaultOpen={true} onOpenChange={() => setOpen(false)}>
-      This is a content
+    <Modal defaultOpen={true} onOpenChange={() => setOpen(false)} size="s">
+      This is a small modal
+    </Modal>
+  );
+};
+export const DefaultOpenModalM = () => {
+  const [, setOpen] = useState(false);
+  return (
+    <Modal defaultOpen={true} onOpenChange={() => setOpen(false)} size="m">
+      This is a medium modal
+    </Modal>
+  );
+};
+export const DefaultOpenModalL = () => {
+  const [, setOpen] = useState(false);
+  return (
+    <Modal defaultOpen={true} onOpenChange={() => setOpen(false)} size="l">
+      This is a large modal
+    </Modal>
+  );
+};
+export const DefaultOpenModalXL = () => {
+  const [, setOpen] = useState(false);
+  return (
+    <Modal defaultOpen={true} onOpenChange={() => setOpen(false)} size="xl">
+      This is a xtra large modal
+    </Modal>
+  );
+};
+export const DefaultOpenModalXXL = () => {
+  const [, setOpen] = useState(false);
+  return (
+    <Modal defaultOpen={true} onOpenChange={() => setOpen(false)} size="xxl">
+      This is a extra-extra large modal
     </Modal>
   );
 };
@@ -32,12 +64,22 @@ export const ModalWithTrigger = () => {
       </Button>
       <Modal
         title="Modal Title"
-        footer={<div className={''}>This is Footer</div>}
+        footer={
+          <div className={'flex gap-x-4 justify-end'}>
+            <Button variant="outline">cancel</Button>
+            <Button>confirm</Button>
+          </div>
+        }
         open={open}
         onOpenChange={() => setOpen(false)}
         elementToFocusOnCloseRef={ref}
       >
-        <div className="dark:text-white">This is a content</div>
+        <div className="dark:text-white">
+          Message: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis velit esse cillum dolore?
+        </div>
       </Modal>
     </>
   );
@@ -52,7 +94,11 @@ export const WithoutTitle = () => {
       <Button color="default" onClick={() => setOpen(true)} ref={ref}>
         Click to open
       </Button>
+      <div className="h-[100px] dark:text-text-input-value">
+        Some text here to check overlay
+      </div>
       <Modal
+        title=" "
         footer={<div className={''}>This is Footer</div>}
         open={open}
         onOpenChange={() => setOpen(false)}
@@ -107,33 +153,6 @@ export const LongContent = () => {
         <div className="dark:text-white">
           {Array.from(Array(20).keys()).map((k) => (
             <p key={k}>This is a content</p>
-          ))}
-        </div>
-      </Modal>
-    </>
-  );
-};
-
-export const FullWidth = () => {
-  const [open, setOpen] = useState(false);
-  return (
-    <>
-      <Button color="default" onClick={() => setOpen(true)}>
-        Click to open
-      </Button>
-      <Modal
-        title="Modal Title"
-        open={open}
-        footer={<div className={''}>This is Footer</div>}
-        onOpenChange={() => setOpen(false)}
-        width="w-full"
-      >
-        <div className="dark:text-white">
-          {Array.from(Array(10).keys()).map((k) => (
-            <p className="text-center" key={k}>
-              It is a long established fact that a reader will be distracted by the
-              readable content of a page when looking at its layout
-            </p>
           ))}
         </div>
       </Modal>
