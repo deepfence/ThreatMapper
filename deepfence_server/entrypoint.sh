1b1cd666de03029b46375c1ef8f1ccab30b4d930
gnmahanth <mahanth@deepfence.io>
2023-06-27 16:27:41 +0000

    move database migrations to scheduler

--- deepfence_server/entrypoint.sh
+++ deepfence_server/entrypoint.sh
@@ -6,17 +6,6 @@ until pg_isready -h "${DEEPFENCE_POSTGRES_USER_DB_HOST}" -p "${DEEPFENCE_POSTGRE
   sleep 5
 done
 
-# Database migration
-/usr/local/bin/migrate \
-  -source file:///usr/local/postgresql-migrate \
-  -database "postgres://${DEEPFENCE_POSTGRES_USER_DB_USER}:${DEEPFENCE_POSTGRES_USER_DB_PASSWORD}@${DEEPFENCE_POSTGRES_USER_DB_HOST}:${DEEPFENCE_POSTGRES_USER_DB_PORT}/${DEEPFENCE_POSTGRES_USER_DB_NAME}?sslmode=${DEEPFENCE_POSTGRES_USER_DB_SSLMODE}" \
-  up
-
-if [ ! $? -eq 0 ]; then
-    echo "postgres database migration failed, exiting"
-    exit 1
-fi
-
 # wait for neo4j to start
 until nc -z ${DEEPFENCE_NEO4J_HOST} ${DEEPFENCE_NEO4J_BOLT_PORT};
 do 
--- deepfence_worker/entrypoint.sh
+++ deepfence_worker/entrypoint.sh
@@ -9,6 +9,20 @@ until pg_isready -h "${DEEPFENCE_POSTGRES_USER_DB_HOST}" -p "${DEEPFENCE_POSTGRE
   sleep 5
 done
 
+# Database migration run if worker moe is scheduler
+databaseURL="postgres://${DEEPFENCE_POSTGRES_USER_DB_USER}:${DEEPFENCE_POSTGRES_USER_DB_PASSWORD}@${DEEPFENCE_POSTGRES_USER_DB_HOST}:${DEEPFENCE_POSTGRES_USER_DB_PORT}/${DEEPFENCE_POSTGRES_USER_DB_NAME}?sslmode=${DEEPFENCE_POSTGRES_USER_DB_SSLMODE}"
+if [ "$DEEPFENCE_MODE" == "scheduler" ]; then
+  echo "run database migrations"
+  /usr/local/bin/migrate -verbose -source file:///usr/local/postgresql-migrate -database $databaseURL up
+  if [ ! $? -eq 0 ]; then
+      echo "postgres database migration failed, exiting"
+      exit 1
+  fi
+  echo "run database migrations, complete"
+else
+  echo "$DEEPFENCE_MODE is not scheduler skip database migrations"
+fi
+
 # wait for neo4j to start
 until nc -z ${DEEPFENCE_NEO4J_HOST} ${DEEPFENCE_NEO4J_BOLT_PORT};
 do 
