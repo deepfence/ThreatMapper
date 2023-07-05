40986c4304dcfea38f3c896b7712492a838f1e28
Manan Vaghasiya <manan@ironmanan.com>
2023-07-05 10:35:52 +0000

    fix issue with navigate

--- deepfence_frontend/apps/dashboard/src/utils/router.ts
+++ deepfence_frontend/apps/dashboard/src/utils/router.ts
@@ -1,7 +1,6 @@
 import {
   defer as routerDefer,
   IndexRouteObject,
-  NavigateFunction,
   NonIndexRouteObject,
 } from 'react-router-dom';
 
@@ -33,5 +32,29 @@ export type CustomRouteObject =
     });
 
 export const historyHelper: {
-  navigate?: NavigateFunction;
-} = {};
+  navigate: (to: string) => void;
+} = {
+  /**
+   * FIXME(manan)
+   * This is a nasty hack!
+   * The problem is, as soon as createBrowserRouter is called, root loader starts
+   * executing immediately. And if auth is not valid, it will try to navigate using
+   * this navigate method, but by that time this navigate method is not assigned with
+   * routers navigate.
+   *
+   * To mitigate this, we have added our makeshift navigate to handle this case. This
+   * WILL do a HARD navigate.
+   *
+   * It appears there is no correct way to mitigate this, hence this hack.
+   * REFRENCES:
+   * https://github.com/remix-run/react-router/issues/9422#issuecomment-1301182219
+   * https://github.com/remix-run/react-router/issues/9422#issuecomment-1305472257
+   * https://jasonwatmore.com/react-router-6-navigate-outside-react-components
+   */
+  navigate: (to: string) => {
+    const newPath = `${window.location.protocol}//${window.location.host}${
+      to.startsWith('/') ? to : `/${to}`
+    }`;
+    window.location.href = newPath;
+  },
+};
