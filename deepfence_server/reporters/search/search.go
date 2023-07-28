3ac1da8d191f92e5d3060fb45eb137a9be28d505
Saurabh Kumar <saurabh@deepfence.io>
2023-07-28 12:30:05 +0530

    Accept empty string as never scanned

--- deepfence_server/reporters/search/search.go
+++ deepfence_server/reporters/search/search.go
@@ -247,12 +247,12 @@ func searchCloudNode(ctx context.Context, filter SearchFilter, fw model.FetchWin
 	}
 	orderFilters := filter.Filters.OrderFilter
 	filter.Filters.OrderFilter = reporters.OrderFilter{}
-	statusKey := "compliance_scan_status"
+	statusKey := "last_scan_status"
 	scanStatusFilterValue, present := filter.Filters.ContainsFilter.FieldsValues[statusKey]
 	scanFilter := reporters.FieldsFilters{}
 	if present {
 		scanFilter.ContainsFilter.FieldsValues = make(map[string][]interface{})
-		scanFilter.ContainsFilter.FieldsValues["last_scan_status"] = scanStatusFilterValue
+		scanFilter.ContainsFilter.FieldsValues[statusKey] = scanStatusFilterValue
 		delete(filter.Filters.ContainsFilter.FieldsValues, statusKey)
 	}
 
@@ -272,8 +272,10 @@ func searchCloudNode(ctx context.Context, filter SearchFilter, fw model.FetchWin
 			ORDER BY s1.updated_at DESC LIMIT 1
 		}
 		CALL {WITH x MATCH (n:` + dummy.NodeType() + `{node_id: x}) RETURN n.node_name as node_name, n.active as active, n.version as version}
-		RETURN x as node_id, node_name, COALESCE(version, 'unknown') as version, compliance_percentage, COALESCE(last_scan_id, '') as last_scan_id, COALESCE(last_scan_status, '') as last_scan_status, active ` + reporters.FieldFilterCypher("", filter.InFieldFilter) +
-		reporters.ParseFieldFilters2CypherWhereConditions("", mo.Some(scanFilter), true) + reporters.OrderFilter2CypherCondition("", orderFilters, nil) + fw.FetchWindow2CypherQuery()
+		WITH x, node_name, version, compliance_percentage, last_scan_id, COALESCE(last_scan_status, '') as last_scan_status, active` +
+		reporters.ParseFieldFilters2CypherWhereConditions("", mo.Some(scanFilter), true) +
+		`RETURN x as node_id, node_name, COALESCE(version, 'unknown') as version, compliance_percentage, COALESCE(last_scan_id, '') as last_scan_id, COALESCE(last_scan_status, '') as last_scan_status, active ` + reporters.FieldFilterCypher("", filter.InFieldFilter) +
+		reporters.OrderFilter2CypherCondition("", orderFilters, nil) + fw.FetchWindow2CypherQuery()
 
 	log.Debug().Msgf("search cloud node query: %v", query)
 	r, err := tx.Run(query,
