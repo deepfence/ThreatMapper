2696b985f5063730a867d59345d064a4b43e8456
Saurabh Kumar <saurabh@deepfence.io>
2023-08-27 23:59:08 +0530

    https://github.com/deepfence/enterprise-roadmap/issues/1933 Fix for splunk payload format

--- deepfence_server/pkg/integration/splunk/splunk.go
+++ deepfence_server/pkg/integration/splunk/splunk.go
@@ -7,6 +7,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -28,37 +29,47 @@ func (s Splunk) SendNotification(ctx context.Context, message string, extras map
 		Timeout: time.Second * 10,
 	}
 
-	// Create a new HEC event with the data
-	event := HECEvent{Event: message}
-
-	// Marshal the HEC event to JSON
-	jsonBytes, err := json.Marshal(event)
-	if err != nil {
-		fmt.Println("Failed to marshal HEC event to JSON", err)
+	var msg []map[string]interface{}
+	d := json.NewDecoder(strings.NewReader(message))
+	if err := d.Decode(&msg); err != nil {
+		fmt.Println("Failed to unmarshal message for splunk", err)
 		return err
 	}
+	for _, payload := range msg {
+		currentMicro := time.Now().UnixMicro()
+		payload["timestamp"] = currentMicro
+		payload["@timestamp"] = currentMicro
+		// Create a new HEC event with the data
+		event := HECEvent{Event: payload}
+		// Marshal the HEC event to JSON
+		jsonBytes, err := json.Marshal(event)
+		if err != nil {
+			fmt.Println("Failed to marshal HEC event to JSON", err)
+			continue
+		}
 
-	// Create a new request to send the JSON data to Splunk
-	req, err := http.NewRequest("POST", s.Config.EndpointURL, bytes.NewBuffer(jsonBytes))
-	if err != nil {
-		fmt.Println("Failed to create HTTP request", err)
-		return err
-	}
-	req.Header.Set("Authorization", "Splunk "+s.Config.Token)
-	req.Header.Set("Content-Type", "application/json")
+		// Create a new request to send the JSON data to Splunk
+		req, err := http.NewRequest("POST", s.Config.EndpointURL, bytes.NewBuffer(jsonBytes))
+		if err != nil {
+			fmt.Println("Failed to create HTTP request", err)
+			continue
+		}
+		req.Header.Set("Authorization", "Splunk "+s.Config.Token)
+		req.Header.Set("Content-Type", "application/json")
 
-	// Send the request to Splunk
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Failed to send data to Splunk", err)
-		return err
-	}
-	defer resp.Body.Close()
+		// Send the request to Splunk
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Println("Failed to send data to Splunk", err)
+			continue
+		}
 
-	// Check the response status code
-	if resp.StatusCode != http.StatusOK {
-		fmt.Println("Failed to send data to Splunk", resp.Status)
-		return errors.New(resp.Status)
+		// Check the response status code
+		if resp.StatusCode != http.StatusOK {
+			fmt.Println("Failed to send data to Splunk", resp.Status)
+			continue
+		}
+		resp.Body.Close()
 	}
 
 	fmt.Println("Data sent to Splunk successfully")
@@ -67,5 +78,5 @@ func (s Splunk) SendNotification(ctx context.Context, message string, extras map
 
 // HECEvent represents an event for the Splunk HTTP Event Collector (HEC) API
 type HECEvent struct {
-	Event string `json:"event"`
+	Event map[string]interface{} `json:"event"`
 }
