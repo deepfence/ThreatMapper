d0ef65c20d5e1d8cbc07099cb75a63e422cc7ae2
gnmahanth <mahanth@deepfence.io>
2023-09-06 13:25:43 +0000

    update server and worker entrypoint to wait for file server

--- deepfence_server/entrypoint.sh
+++ deepfence_server/entrypoint.sh
@@ -24,10 +24,18 @@ do
   sleep 5; 
 done
 
+# wait for kafka connection
 until kcat -L -b ${DEEPFENCE_KAFKA_BROKERS};
 do
   echo "kafka is unavailable - sleeping"
-  sleep 5
+  sleep 5;
+done
+
+# wait for file server to start
+until nc -z ${DEEPFENCE_MINIO_HOST} ${DEEPFENCE_MINIO_PORT};
+do
+  echo "file server is unavailable - sleeping"
+  sleep 5;
 done
 
 sed -i "s/https:\/\/petstore.swagger.io\/v2\/swagger.json/\/deepfence\/openapi.json/g" /usr/local/share/swagger-ui/swagger-initializer.js
