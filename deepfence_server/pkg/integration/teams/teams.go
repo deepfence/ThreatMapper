780f8a5c17c2bb83837f54111352ad1c2a659578
Saurabh Kumar <saurabh@deepfence.io>
2023-09-06 19:10:17 +0530

    teams slice index fix

--- deepfence_server/pkg/integration/teams/teams.go
+++ deepfence_server/pkg/integration/teams/teams.go
@@ -44,6 +44,9 @@ func (t Teams) SendNotification(ctx context.Context, message string, extras map[
 
 	startIndex := 0
 	endIndex := BatchSize
+	if endIndex > len(msg) {
+		endIndex = len(msg)
+	}
 	if err := t.sendNotification(msg[startIndex:endIndex]); err != nil {
 		return err
 	}
