2f8c999be7a7d1b2c249ee0334a147733bf1df7d
Harshvardhan Karn <harshvardhan@deepfence.io>
2023-12-07 07:45:16 +0000

    remove default password from config

--- deepfence_postgres/postgres-entrypoint.sh
+++ deepfence_postgres/postgres-entrypoint.sh
@@ -4,8 +4,18 @@ echoerr() { echo "$@" 1>&2; }
 
 export POSTGRES_MULTIPLE_DATABASES="$DEEPFENCE_POSTGRES_USER_DB_NAME"
 export POSTGRES_USER=$DEEPFENCE_POSTGRES_USER_DB_USER
+if [ -z "$POSTGRES_USER" ]; then
+    export POSTGRES_USER="deepfence"
+fi
 export POSTGRES_PASSWORD=$DEEPFENCE_POSTGRES_USER_DB_PASSWORD
+if [ -z "$POSTGRES_PASSWORD" ]; then
+    export POSTGRES_PASSWORD="deepfence"
+fi
+
 export PGPASSWORD=$DEEPFENCE_POSTGRES_USER_DB_PASSWORD
+if [ -z "$PGPASSWORD" ]; then
+    export PGPASSWORD="deepfence"
+fi
 
 /bin/bash /usr/local/bin/create-pg-dirs.sh && /bin/bash /usr/local/bin/new-docker-entrypoint.sh postgres -c shared_buffers=256MB -c max_connections=1000
 
