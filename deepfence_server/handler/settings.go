a0c5273e8d431305f0a1025e8d1d0e6b3bbf489e
ramanan-ravi <ramanan@deepfence.io>
2024-02-01 21:19:20 +0530

    Validation for console URL in settings deepfence/enterprise-roadmap#2105

--- deepfence_server/handler/settings.go
+++ deepfence_server/handler/settings.go
@@ -20,12 +20,9 @@ import (
 )
 
 var (
-	errInvalidID = ValidatorError{
-		err: errors.New("id:invalid id"), skipOverwriteErrorMessage: true}
-	errInvalidURL = ValidatorError{
-		err: errors.New("value:invalid url"), skipOverwriteErrorMessage: true}
-	errInvalidInteger = ValidatorError{
-		err: errors.New("value:must be integer"), skipOverwriteErrorMessage: true}
+	errInvalidID              = BadDecoding{err: errors.New("invalid id")}
+	errInvalidURL             = BadDecoding{err: errors.New("invalid url")}
+	errInvalidInteger         = BadDecoding{err: errors.New("must be integer")}
 	errInvalidEmailConfigType = ValidatorError{
 		err: fmt.Errorf("email_provider:must be %s or %s", model.EmailSettingSMTP, model.EmailSettingSES), skipOverwriteErrorMessage: true}
 )
