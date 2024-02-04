3838d23f5332a8cb994c2dd53abdad7b069e7082
milan-deepfence <rkmilansingh@gmail.com>
2024-02-04 19:44:16 +0530

    boolean or string true to allow

--- deepfence_frontend/apps/dashboard/vite.config.ts
+++ deepfence_frontend/apps/dashboard/vite.config.ts
@@ -13,37 +13,37 @@ const current = fileURLToPath(import.meta.url);
 const root = path.dirname(current);
 
 const matomoPlugin = (enable: string) => {
-  if (!enable) {
+  if (enable.trim() === 'true') {
     return {
       name: 'analytics-tracking',
       transformIndexHtml(html) {
-        return html.replace(/<analytics-tracking>(.*?)<\/analytics-tracking>/, '');
+        return html.replace(
+          /<analytics-tracking>(.*?)<\/analytics-tracking>/,
+          `<script>
+          var _paq = (window._paq = window._paq || []);
+          /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+          _paq.push(['trackPageView']);
+          _paq.push(['enableLinkTracking']);
+          (function () {
+            var u = '//analytics.deepfence.io/';
+            _paq.push(['setTrackerUrl', u + 'matomo.php']);
+            _paq.push(['setSiteId', '1']);
+            var d = document,
+              g = d.createElement('script'),
+              s = d.getElementsByTagName('script')[0];
+            g.async = true;
+            g.src = u + 'matomo.js';
+            s.parentNode.insertBefore(g, s);
+          })();
+        </script>`,
+        );
       },
     };
   }
   return {
     name: 'analytics-tracking',
     transformIndexHtml(html) {
-      return html.replace(
-        /<analytics-tracking>(.*?)<\/analytics-tracking>/,
-        `<script>
-        var _paq = (window._paq = window._paq || []);
-        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-        _paq.push(['trackPageView']);
-        _paq.push(['enableLinkTracking']);
-        (function () {
-          var u = '//analytics.deepfence.io/';
-          _paq.push(['setTrackerUrl', u + 'matomo.php']);
-          _paq.push(['setSiteId', '1']);
-          var d = document,
-            g = d.createElement('script'),
-            s = d.getElementsByTagName('script')[0];
-          g.async = true;
-          g.src = u + 'matomo.js';
-          s.parentNode.insertBefore(g, s);
-        })();
-      </script>`,
-      );
+      return html.replace(/<analytics-tracking>(.*?)<\/analytics-tracking>/, '');
     },
   };
 };
