6cbb11917cabe66f9dc95388eeacaae9f0c03f9c
gnmahanth <mahanth@deepfence.io>
2024-02-21 10:40:17 +0000

    remove plugins directory, update pre and post upgrade scripts

--- deepfence_neo4j/post-upgrade-to-v5.sh
+++ deepfence_neo4j/post-upgrade-to-v5.sh
@@ -2,12 +2,33 @@
 
 set -e
 
-neo4j stop
+NEO4J_MAJOR_VERSION=`neo4j version | cut -d" " -f2 | cut -d. -f1`
 
-mkdir -p /backups/migrations
+echo "neo4j version: $NEO4J_MAJOR_VERSION"
 
-neo4j-admin database load --expand-commands neo4j --from-path=/backups/migrations/ --overwrite-destination=true
+if [ "$NEO4J_MAJOR_VERSION" == "5" ]
+then
 
-neo4j-admin database migrate neo4j --force-btree-indexes-to-range
+    # check if migration is required
+    if [ -e /backups/migrations/.migrate ]
+    then
+        echo "database migration required for new version"
 
-neo4j start
+        neo4j stop
+
+        neo4j-admin database load --expand-commands neo4j --from-path=/backups/migrations/ --overwrite-destination=true
+
+        neo4j-admin database migrate neo4j --force-btree-indexes-to-range
+
+        neo4j start
+
+        rm /backups/migrations/.migrate
+
+        echo "migration complete"
+    else
+        echo "skip migration already completed"
+    fi
+
+else
+    echo "no migration required"
+fi
--- deepfence_neo4j/pre-upgrade-to-v5.sh
+++ deepfence_neo4j/pre-upgrade-to-v5.sh
@@ -2,12 +2,25 @@
 
 set -e
 
-neo4j stop
+NEO4J_MAJOR_VERSION=`neo4j version | cut -d" " -f2 | cut -d. -f1`
 
-mkdir -p /backups/migrations
+echo "neo4j version: $NEO4J_MAJOR_VERSION"
 
-neo4j-admin dump --expand-commands --database=system --to /backups/migrations/system.dump 
+if [ "$NEO4J_MAJOR_VERSION" == "4" ]
+then
+    echo "prepare for database migration required for new version"
 
-neo4j-admin dump --expand-commands --database=neo4j --to /backups/migrations/neo4j.dump
+    neo4j stop
 
-rm -rf /data/databases/* /data/transactions/*
+    mkdir -p /backups/migrations
+
+    neo4j-admin dump --expand-commands --database=system --to /backups/migrations/system.dump 
+
+    neo4j-admin dump --expand-commands --database=neo4j --to /backups/migrations/neo4j.dump
+
+    rm -rf /data/databases/* /data/transactions/* /plugins/*
+
+    touch /backups/migrations/.migrate
+else
+    echo "skip migration preparation"
+fi
