be80f6718ea3e2b0a40119ad2f3bb57103bdec4d
Thomas Legris <thomas@deepfence.io>
2024-06-10 22:29:28 +0900

    fix neo4j dump/load & backup script

--- deepfence_neo4j/backup_neo4j.sh
+++ deepfence_neo4j/backup_neo4j.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
-
-dt=$(date '+%Y-%m-%d_%H-%M-%S');
-BACKUP_FILE="/backups/neo4j_backup_$dt.dump"
-
-echo "Backup file is: $BACKUP_FILE"
-
-mkdir -p /backups/
-
-neo4j stop
-retVal=$?
-if [ $retVal -ne 0 ]; then
-    echo "Failed to stop the neo4j db"
-    exit
-fi
-
-neo4j-admin database dump neo4j --to-stdout > $BACKUP_FILE
-retVal=$?
-if [ $retVal -ne 0 ]; then
-    echo "Failed to create the backup file"
-fi                             
-
-sleep 2s
-/startup/docker-entrypoint.sh neo4j >& /dev/null&
+kill -USR1 1
+sleep 10
+while ! neo4j status 1>&2 2>/dev/null; do
+  echo "Waiting for neo4j to be up..."
+  sleep 5
+done
