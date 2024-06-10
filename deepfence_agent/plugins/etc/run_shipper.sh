0f029f19df028f3db28db5ce4e48ca89a172947b
Ramanan Ravikumar <38394463+ramanan-ravi@users.noreply.github.com>
2024-06-10 19:50:12 +0530

    Fetch cloud resources periodically (#2191)

--- deepfence_agent/plugins/etc/run_shipper.sh
+++ deepfence_agent/plugins/etc/run_shipper.sh
@@ -1,5 +1,6 @@
 BATCH_SIZE=${1:-100}
+TRUNCATE_SIZE=${2:-10}
 
 PATH=${DF_INSTALL_DIR:-/home/deepfence}
 
-exec $PATH/bin/shipper --base-path="${DF_INSTALL_DIR:-/}" --truncate-size=2 --routes=$PATH/routes.yaml --batch-size=$BATCH_SIZE
+exec $PATH/bin/shipper --base-path="${DF_INSTALL_DIR:-/}" --truncate-size=$TRUNCATE_SIZE --routes=$PATH/routes.yaml --batch-size=$BATCH_SIZE
