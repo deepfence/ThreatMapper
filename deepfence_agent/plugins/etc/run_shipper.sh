54ae62134b50de9ecaef47584ec4498749bf0403
Ramanan Ravikumar <38394463+ramanan-ravi@users.noreply.github.com>
2024-07-26 23:04:25 +0530

    Shipper path in linux agent binary (#2274)

--- deepfence_agent/plugins/etc/run_shipper.sh
+++ deepfence_agent/plugins/etc/run_shipper.sh
@@ -1,6 +1,4 @@
 BATCH_SIZE=${1:-100}
 TRUNCATE_SIZE=${2:-10}
 
-PATH=${DF_INSTALL_DIR:-/home/deepfence}
-
-exec $PATH/bin/shipper --base-path="${DF_INSTALL_DIR:-/}" --truncate-size=$TRUNCATE_SIZE --routes=$PATH/routes.yaml --batch-size=$BATCH_SIZE
+exec $DF_INSTALL_DIR/home/deepfence/bin/shipper --base-path="${DF_INSTALL_DIR:-/}" --truncate-size=$TRUNCATE_SIZE --routes=$DF_INSTALL_DIR/home/deepfence/routes.yaml --batch-size=$BATCH_SIZE
