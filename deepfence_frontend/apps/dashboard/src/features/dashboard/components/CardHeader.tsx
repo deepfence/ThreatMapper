86d6644f2d3f4dbcd5afc169d297314ba32d6fbf
Milan <111341101+milan-deepfence@users.noreply.github.com>
2024-07-30 15:22:30 +0530

    Update card headers for landing pages (#2277)
    * Update card headers for landing pages
    
    * remove affected resource column from secret and malware

--- deepfence_frontend/apps/dashboard/src/features/dashboard/components/CardHeader.tsx
+++ deepfence_frontend/apps/dashboard/src/features/dashboard/components/CardHeader.tsx
@@ -15,17 +15,20 @@ export const CardHeader = ({
   const Wrapper = ({ children }: { children: ReactNode }) => {
     if (link?.length) {
       return (
-        <DFLink
-          to={link}
-          unstyled
-          className="p-3 flex items-center text-text-text-and-icon hover:text-accent-accent border-b dark:border-df-gray-900 border-bg-grid-border"
+        <div
+          className="p-3 flex items-center text-text-text-and-icon border-b dark:border-df-gray-900 border-bg-grid-border"
           data-testid={`${title}Id`}
         >
           <span className="flex-1 truncate flex items-center gap-2">{children}</span>
-          <div className="h-4 w-4 shrink-0 -rotate-90 justify-self-end">
-            <CaretDown />
-          </div>
-        </DFLink>
+          <DFLink to={link}>
+            <div className="text-p4 leading-0 flex gap-x-1 items-center">
+              More
+              <div className="h-4 w-4 shrink-0 -rotate-90 justify-self-end -mr-1 hover:underline">
+                <CaretDown />
+              </div>
+            </div>
+          </DFLink>
+        </div>
       );
     }
     return (
--- deepfence_frontend/apps/dashboard/src/features/malwares/pages/landing/CardHeader.tsx
+++ deepfence_frontend/apps/dashboard/src/features/malwares/pages/landing/CardHeader.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from 'react';
 
 import { DFLink } from '@/components/DFLink';
 import { CaretDown } from '@/components/icons/common/CaretDown';
+import { TruncatedText } from '@/components/TruncatedText';
 
 export const CardHeader = ({
   icon,
@@ -13,16 +14,17 @@ export const CardHeader = ({
   path: string;
 }) => {
   return (
-    <DFLink to={path} unstyled>
-      <div className="flex items-center gap-2 text-text-text-and-icon hover:text-accent-accent border-b dark:border-df-gray-900 border-bg-grid-border px-2 py-3">
-        <span className="h-4 w-4 shrink-0">{icon}</span>
-        <span className="text-t2 uppercase truncate flex-1" title={title}>
-          {title}
-        </span>
-        <div className="h-4 w-4 shrink-0 -rotate-90 justify-self-end">
-          <CaretDown />
+    <div className="flex items-center gap-2 text-text-text-and-icon border-b dark:border-df-gray-900 border-bg-grid-border px-2 py-3">
+      <span className="h-4 w-4 shrink-0">{icon}</span>
+      <TruncatedText className="text-t2 uppercase truncate flex-1" text={title} />
+      <DFLink to={path}>
+        <div className="text-p4 leading-0 flex gap-x-1 items-center">
+          More
+          <div className="h-4 w-4 shrink-0 -rotate-90 justify-self-end -mr-1 hover:underline">
+            <CaretDown />
+          </div>
         </div>
-      </div>
-    </DFLink>
+      </DFLink>
+    </div>
   );
 };
