650a82c3e54a752b0fb29514e4c968184f6ef195
ramanan-ravi <ramanan@deepfence.io>
2024-08-03 15:07:48 +0530

    Create cloud scanner log directories on restart

--- deepfence_agent/plugins/etc/run_shipper.sh
+++ deepfence_agent/plugins/etc/run_shipper.sh
@@ -1,4 +1,10 @@
-BATCH_SIZE=${1:-100}
-TRUNCATE_SIZE=${2:-10}
+#!/bin/bash
 
-exec $DF_INSTALL_DIR/home/deepfence/bin/shipper --base-path="${DF_INSTALL_DIR:-/}" --truncate-size=$TRUNCATE_SIZE --routes=$DF_INSTALL_DIR/home/deepfence/routes.yaml --batch-size=$BATCH_SIZE
+export BATCH_SIZE=${1:-100}
+export TRUNCATE_SIZE=${2:-10}
+
+if [[ $DF_INSTALL_DIR == "/home/deepfence" ]]; then
+  exec /home/deepfence/bin/shipper --base-path="${DF_INSTALL_DIR:-/}" --truncate-size=$TRUNCATE_SIZE --routes=/home/deepfence/routes.yaml --batch-size=$BATCH_SIZE
+else
+  exec $DF_INSTALL_DIR/home/deepfence/bin/shipper --base-path="${DF_INSTALL_DIR:-/}" --truncate-size=$TRUNCATE_SIZE --routes=$DF_INSTALL_DIR/home/deepfence/routes.yaml --batch-size=$BATCH_SIZE
+fi
